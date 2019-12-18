70d5aa4f9b36cb1d022d460eab142ecf8dc1001d
Zhao Qi <qi.zhao1991@gmail.com>
2019-12-18 21:32:48 +0100

    Update utils.py

--- kmeans/utils.py
+++ kmeans/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def load_data(file_path):
     """load dataset
     """
@@ -8,5 +11,21 @@ def load_data(file_path):
     for i in range(len(contents)):
         data.append(contents[i][:]
         
-    return np.array(data).astype(float)
-    
+    return np.array(data).astype(float)           
+
+def show_cluster(data, k, centroids, cluster_assment):  
+    n_samples, dim = data.shape  
+    if dim != 2:  
+        print("Sorry! I can not draw because the dimension of your data is not 2!")
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']  
+    if k > len(mark):  
+        print("Sorry! Your k is too large! please contact Zouxy")
+    # draw all samples  
+    for i in range(n_samples):  
+        mark_index = int(cluster_assment[i, 0])  
+        plt.plot(data[i, 0], data[i, 1], mark[mark_index])  
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']  
+    # draw the centroids  
+    for i in range(k):  
+        plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize = 12)  
+    plt.show()  
