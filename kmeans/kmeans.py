b97a1eacade3b0dd404b47f1f732b596ed4a5dd6
Zhao Qi <qi.zhao1991@gmail.com>
2019-12-18 21:32:12 +0100

    Update kmeans.py

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -51,26 +51,3 @@ class KMeans(object):
                 pts_in_cluster[cent,:] = mean(pts_in_cluster,axis=0)
         return centroid, cluster_assment
 
- 
-            
-
-def show_cluster(data, k, centroids, cluster_assment):  
-    n_samples, dim = data.shape  
-    if dim != 2:  
-        print("Sorry! I can not draw because the dimension of your data is not 2!")
-    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']  
-    if k > len(mark):  
-        print("Sorry! Your k is too large! please contact Zouxy")
-    # draw all samples  
-    for i in range(n_samples):  
-        mark_index = int(cluster_assment[i, 0])  
-        plt.plot(data[i, 0], data[i, 1], mark[mark_index])  
-    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']  
-    # draw the centroids  
-    for i in range(k):  
-        plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize = 12)  
-    plt.show()  
-
-
-
-
