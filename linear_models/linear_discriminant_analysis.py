1b44b5635462dfde917ac45e1d0a9eda75a1444f
Zhao Qi <qi.zhao1991@gmail.com>
2020-01-08 23:31:17 +0100

    Update linear_discriminant_analysis.py

--- linear_models/linear_discriminant_analysis.py
+++ linear_models/linear_discriminant_analysis.py
@@ -6,11 +6,8 @@ def compute_class_mean(data, label, n_classes):
 
     Args
     ----------
-        data : array-like, shape (n_samples, n_features)
-            Input data.
-        label : array-like, shape (n_samples,) or (n_samples, n_targets)
-            Target values.
-        
+        data : array-like, shape (n_samples, n_features) Input data.
+        label : array-like, shape (n_samples,) or (n_samples, n_targets) Target values.
         n_classes : int, number of class 
         
     Returns
@@ -55,4 +52,32 @@ def compute_within_class_Sw(data, label, n_classes):
         s_w += class_cov
     return s_w
 
+def compute_between_class_Sb(data, label, n_classes):
+    """Compute between class covariance matrix
+
+    Args
+    ----------
+        data : array-like, shape (n_samples, n_features) Input data.
+        label : array-like, shape (n_samples,) or (n_samples, n_targets) Target values.
+        n_classes : int, number of class 
+        
+    Returns
+    -------
+        s_b : array-like, shape (n_features, n_features) between class covariance matrix.
+
+    """
+    n_samples, n_features = np.shape(data)
+    total_mean = np.mean(data, axis=0)
+    s_b = np.zeros((n_features, n_features), dtype=float)
+
+    class_means = compute_class_mean(data, label, n_classes)
+
+    for n_class_mean, class_mean in enumerate(class_means):
+        n_class = data[label == n_class_mean].shape[0]
+        class_mean = np.reshape(class_mean, [n_features, 1])
+        total_mean = np.reshape(total_mean, [n_features, 1])
+
+        s_b += n_class * np.dot((class_mean - total_mean), np.transpose(class_mean - total_mean))
+
+    return s_b
 
