c4ce6f7dda3d7f2b83b0c1ce4a1c8b77d3da58e2
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-01 17:24:25 +0100

    Update linear_regression.py

--- linear_models/linear_regression.py
+++ linear_models/linear_regression.py
@@ -15,7 +15,68 @@ def load_data(path):
         label.append(lines[i][-1])
     
     return np.array(data, dtype=np.float), np.array(label, dtype=np.float)
+
+
+class LinearRegression(object):
+    """LinearRegression fits a linear model with coefficients w = (w1, ..., wp)
+    to minimize the residual sum of squares between the observed targets in
+    the dataset, and the targets predicted by the linear approximation.
+
+    Args:
+        normalize : bool, optional, default False. If True, the regressors X will be normalized 
+        before regression by subtracting the mean and dividing by the l2-norm
+
+    """
+    def __init__(self, normalize=True):
+        self.normalize = normalize
+
+    def _feature_normalize(self, X):
+        """normalize data by substracting the mean and dividing sigma
+        """
+        if not isinstance(X, np.ndarray):
+            raise ValueError('Input data is not ndarray type')
+        
+        n_samples, n_features = X.shape
+        x_norm = np.zeros((n_samples, n_features), dtype=X.dtype)
+
+        mu = np.mean(X, axis=0)
+        sigma = np.std(X, axis=0)
+
+        for i in range(n_features):
+            x_norm[:, i] = (X[:, i] - mu[i])/sigma[i]
+
+        return x_norm
     
+    def _compute_cost(self, X, y, theta):
+        """compute cost function, e.g. f(x) = (1/N)*{(wx-y)'(wx_y)}
+        """
+        n_samples = len(y)
+        return (np.transpose(X*theta-y))*(X*theta-y)/(2*n_samples)
+    
+    def _compute_gradient_descent(X, y, theta, alpha, max_steps=100):
+        """Compute gradient descent to optimize vals of function
+        Args:
+            X: ndarray-like data, input data
+            y: ndarray_like, label
+            theta: model weights
+            alpha: float, learning rate when update weights
+            max_steps: int, max iterations to update weight
+        
+        Returns:
+            theta and cost of object function J_history
+
+        """
+        n_samples, n_features = X.shape
+
+
+    def test(self, X):
+        return self._feature_normalize(X)
+
+
+
+
+
+ 
 def linear_regression(X, y):
     """standard linear regression function
     Args:
