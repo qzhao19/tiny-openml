c49d10b0adb4dfa635529f1892f65b0840892262
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-02 20:59:42 +0100

    Update linear_regression.py

--- linear_models/linear_regression.py
+++ linear_models/linear_regression.py
@@ -46,14 +46,14 @@ class LinearRegression(object):
             x_norm[:, i] = (X[:, i] - mu[i])/sigma[i]
 
         return x_norm
-
+    
     def _compute_cost(self, X, y, theta):
         """compute cost function, e.g. f(x) = (1/N)*{(wx-y)'(wx_y)}
         """
         n_samples = len(y)
         return (np.transpose(X*theta-y))*(X*theta-y)/(2*n_samples)
     
-    def _compute_gradient_descent(X, y, theta, alpha, max_steps=100):
+    def _compute_gradient_descent(self, X, y, theta, alpha, n_iters=100):
         """Compute gradient descent to optimize vals of function
         Args:
             X: ndarray-like data, input data
@@ -66,55 +66,33 @@ class LinearRegression(object):
             theta and cost of object function J_history
 
         """
-        n_samples, n_features = X.shape
+        n_samples = len(y)
+        n_weights = len(theta)
+
+        # temp variabel to keeping theta of each iteration computation
+        temps = np.matrix(np.zeros((n_weights, n_iters), dtype=np.float))
+        J_history = np.zeros((n_iters, 1), dtype=np.float)
+
+        for i in range(n_iters):
+            # compute the product of X and weights
+            h = np.dot(X, theta)
+            # compute gradients
+            temps[:, i] = theta - (alpha/n_samples)*(np.dot(np.transpose(X), h-y))
+            theta = temps[:, i]
+            # call object cost function 
+            J_history = self._compute_cost(X, y, theta)
+            print('.', end=' ')      
+        return theta, J_history 
+
+
+
+
+
+
+
 
 
     def test(self, X):
         return self._feature_normalize(X)
 
 
-
-
-
- 
-def linear_regression(X, y):
-    """standard linear regression function
-    Args:
-        X: float, dataset
-        y: float, label
-    """
-    # make sur that matrix shoule be full rank, which means X.T * X inversible
-    if np.linalg.det(np.dot(np.transpose(X), X)) == 0:
-        raise ValueError('This matrix is singular, cannot do inverse')
-    w = np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), (np.dot(np.transpose(X), y)))
-    return w
-
-def plot_dataset(X, y):
-    """plot Scatter figure
-    
-    """
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(X[:, 1], y, s=20, c='blue', alpha=0.5)
-    plt.title('DataSet')
-    plt.xlabel("X")
-    plt.xlabel("y")
-    plt.show()
-    
-def plot_regression(X, y, ws):
-    """plot linear regression equation
-    
-    """
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(X[:,1], np.dot(X, ws), c = 'red') 
-    ax.scatter(X[:,1], y, s = 20, c = 'blue', alpha = .5)
-    plt.title('DataSet')
-    plt.xlabel("X")
-    plt.ylabel("y")
-    plt.show()
-    
-if __name__ == "__main__":
-    data, label = load_data('./dataset1.txt') 
-    coefs = linear_regression(data, label)       
-    plot_regression(data, label, coefs)
