2d880ca89a3ed074b75d57b5453957c2e4ac3b14
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-10 22:18:48 +0100

    Update utils.py

--- linear_models/utils.py
+++ linear_models/utils.py
@@ -165,3 +165,39 @@ def plot_regression(X, y, ws):
 #         error_rate = error/n_samples
 #         print ("error rate is:", "%.4f" %error_rate)
 #         return error_rate
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# class RidgeRegression(object):
+#     """Linear least squares with l2 regularization.
+
+#     This model solves a regression model where the loss function is
+#     the linear least squares function and regularization is given by
+#     the l2-norm. Also known as Ridge Regression or Tikhonov regularization.
+#     This estimator has built-in support for multi-variate regression
+#     (i.e., when y is a 2d-array of shape [n_samples, n_targets]).
+
+#     """
+#     def __init__(self, alpha):
+#         self.alpha = alpha
+
+#     def fit(self, X, y):
+#         """fit data
+#         Args:
+#             X: ndarray-data. training data with shape (n_samples, n_features)
+#             y: ndarray-data. label with shape (n_samples, 1)
+
+#         Returns:
+#             coefs: w
+#         """
+#         n_samples, n_features = X.shape
+
+#         # print(np.dot(np.transpose(X), X).shape)
+#         w = np.dot(np.linalg.inv(np.dot(np.transpose(X), X) + self.alpha*np.eye(n_features, n_features)), np.dot(np.transpose(X), y))
+#         return w
