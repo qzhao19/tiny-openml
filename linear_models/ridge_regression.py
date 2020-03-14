f6b4e64528d59b628286729763803ce5e11ae039
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-14 23:24:12 +0100

    Update ridge_regression.py

--- linear_models/ridge_regression.py
+++ linear_models/ridge_regression.py
@@ -1,28 +1,10 @@
 import numpy as np
-from scipy import linalg
-
-def ridge(A, b, alphas):
-    """
-    Return coefficients for regularized least squares
-
-         min ||A x - b||^2 + alpha ||x||^2
-
-    Parameters
-    ----------
-    A : array, shape (n, p)
-    b : array, shape (n,)
-    alphas : array, shape (k,)
-
-    Returns
-    -------
-    coef: array, shape (p, k)
-    """
-    U, s, Vt = linalg.svd(X, full_matrices=False)
-    d = s / (s[:, np.newaxis].T ** 2 + alphas[:, np.newaxis])
-    return np.dot(d * U.T.dot(y), Vt).T
+from scipy.sparse import linalg as sp_linalg
 
 
-class RidgeRegression(object):
+
+
+class Ridge(object):
     """Linear least squares with l2 regularization.
     
     Minimizes the objective function: ||y - Xw||^2_2 + alpha * ||w||^2_2
@@ -37,14 +19,124 @@ class RidgeRegression(object):
            improves the conditioning of the problem and reduces the variance of
            the estimates. Larger values specify stronger regularization.
            
-       solver: 
+       normalize: boolean, optional, default False
+           If True, the regressors X will be normalized before regression.
+           
+        solver: string, default value is {auto, svd}
+            Solver to use in the computational routines:
+            
     
     
     """
-    def __init__(self, C = 1.0, solver = 'lsqr'):
-        self._C = C
+
+    def __init__(self, alpha = 1.0, normalize = False, solver = 'auto'):
+        self._alpha = alpha
+        self._normalize = normalize
         self._solver = solver
-    
-    def _cost_fn(self, theta, X, y)
         
+    def _normalize(self, X, y):
+        """Normalize data 
+        
+            X = (X - mu)/sigma
+            y = y - mu
+        
+
+        Parameters
+        ----------
+            X : ndarray of shape [n_samples, n_features]
+                Training vector.
+            y : ndarray of shape [n_samples, 1]
+                Target label vector.
+
+        Returns
+        -------
+            Normalized training matrix and target vector
+
+        """
+        
+        return (X - np.mean(X, axis = 1))/np.std(X, axis=1), y - np.mean(y)
+
+
+    def _solve_sparse_cg(self, X, y, max_iter=None, tol = 1e-3):
+        """
+
+        Parameters
+        ----------
+        X : TYPE
+            DESCRIPTION.
+        y : TYPE
+            DESCRIPTION.
+        max_iter : TYPE, optional
+            DESCRIPTION. The default is None.
+        tol : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        return 0
+
+
+# def ridge(A, b, alphas):
+#     """Return coefficients for regularized least squares
+
+#          min ||A x - b||^2 + alpha ||x||^2
+
+#     Parameters
+#     ----------
+#     A : array, shape (n, p)
+#     b : array, shape (n,)
+#     alphas : array, shape (k,)
+
+#     Returns
+#     -------
+#     coef: array, shape (p, k)
+#     """
+#     U, s, Vt = linalg.svd(X, full_matrices=False)
+#     d = s / (s[:, np.newaxis].T ** 2 + alphas[:, np.newaxis])
+#     return np.dot(d * U.T.dot(y), Vt).T
+
+
+# class RidgeRegression(object):
+#     """Linear least squares with l2 regularization.
+    
+#     Minimizes the objective function: ||y - Xw||^2_2 + alpha * ||w||^2_2
+#     This model solves a regression model where the loss function is
+#     the linear least squares function and regularization is given by
+#     the l2-norm.
+    
+#     Parameters
+#     ----------
+#         C: float, default is 1.0
+#            Regularization strength; must be a positive float. Regularization
+#            improves the conditioning of the problem and reduces the variance of
+#            the estimates. Larger values specify stronger regularization.
+           
+#        solver: 
+    
+    
+#     """
+#     def __init__(self, alpha = 1.0, solver = 'lsqr'):
+#         self._alpha = alpha
+#         self._solver = solver
+    
+#     def _cost_fn(self, theta, X, y):
+#         """compute cost function, e.g. f(x) = (1/N)*{(wx-y)'(wx_y)}
+#         """
+#         n_samples = len(y)
+#         return (np.transpose(X * theta - y)) * (X * theta - y)/(2 * n_samples) + self._alpha * np.dot(np.transpose(theta), theta)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
