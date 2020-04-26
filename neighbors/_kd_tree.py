ab3e3fb2352cf2fd50993c3dbe2b3238ca0bdb53
zhaoqi19 <qi.zhao1991@gmail.com>
2020-04-26 23:22:36 +0200

    update KD tree file

--- neighbors/_kd_tree.py
+++ neighbors/_kd_tree.py
@@ -31,7 +31,23 @@ class Node(object):
         self.right = right
     
     
-    
+    @property
+    def children(self):
+        """returns an iterator for the non-empty child of the node
+        Children are returned as a tuple of (node, position) where 
+        position is 0 for the left subnode and 1 for right subnode 
+        
+
+        Returns
+        -------
+            iterator of a tuple (node, position).
+
+        """
+        if self.left and self.left.data is not None:
+            yield self.left, 0
+        
+        if self.right and self.right.data is not None:
+            yield self.right, 1
 
 
 
