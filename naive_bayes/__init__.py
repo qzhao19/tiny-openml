e72bafd049dd0a48b29ee6e4b081bb48394606a9
qzhao19 <qi.zhao@outlook.fr>
2021-06-14 23:28:32 +0800

    delete

--- naive_bayes/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 20 23:17:00 2020
-
-@author: qizhao
-"""
-
-
--- svm/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 31 22:07:23 2020
-
-@author: qizhao
-"""
-
-
