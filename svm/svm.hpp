987cf2e6b110b994aefd3e0be5d549c0156315d8
qzhao19 <qi.zhao@outlook.fr>
2021-06-22 23:24:33 +0800

    update svm.hpp, add Cache class

--- svm/svm.hpp
+++ svm/svm.hpp
@@ -15,20 +15,40 @@ typedef signed char schar;
 
 class Cache {
 public:
-    Cache(int n_samples_, int size_);
+    // according the n_samples, we alllocate the memory space where have the same n_samples
+    Cache(int n_samples_, int size_);       
     ~Cache();
+
+    int get_data(const int index, Qfloat **data, int len);
+
+
+    // swap values from two head_t 
+    void swap_index();                      
 
 
 private:
-    int n_samples;
-    int size;
+    int n_samples;                  // the number of data samples
+    int size;                       // 指定的全部内存
+
+    //结构head_t用来记录所申请内存的指针，并记录长度，而且通过双向的指针，形成链表，增加寻址的速度
     struct head_t {
-        head_t *prev, *next;
+        head_t *prev, *next;        
         Qfloat *data;
         int len;
     };
 
-    
+    // 变量指针，该指针记录程序中所申请的内存。
+    head_t *head;
+
+    // the head node of doubled linked list
+    head_t lru_head;
+
+    // 从双向链表中删去某个元素的链接，一般是删去当前所指向的元素
+    void lru_delete(head_t *h);
+
+    // 在链表后面插入一个新的链接
+    void lru_insert(head_t *h);
+
 
 
 
