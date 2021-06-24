a60ebcdc30a3fc0933bb8954c7a288a04cd7d328
qzhao19 <qi.zhao@outlook.fr>
2021-06-24 17:51:32 +0800

    Update svm_node.hpp

--- svm/svm_node.hpp
+++ svm/svm_node.hpp
@@ -2,12 +2,13 @@
 #define SVM_NODE_HPP_
 
 struct svm_node {
-    /*store the single feature in a single vector ,
-    for example, x1 = {0.5, 0.2, 0.6, 0.8}; so if we use 
-    svm_node to store it, which is a array what contains 5 
-    svm_node, the memory map is:
-            1      2      3     4     -1
-            0.5    0.2    0.6   0.8    null
+    /**
+     * store the single feature in a single vector ,
+     * for example, x1 = {0.5, 0.2, 0.6, 0.8}; so if we use 
+     * svm_node to store it, which is a array what contains 5 
+     * svm_node, the memory map is:
+     *       1      2      3     4     -1
+     *       0.5    0.2    0.6   0.8    null
     */
     int index;
     double value;
