7feceba388818ed6aa743a965ca1703d59a56278
qzhao19 <qi.zhao@outlook.fr>
2021-06-25 23:11:34 +0800

    update svm_data.cpp

--- svm/svm_data.hpp
+++ svm/svm_data.hpp
@@ -6,10 +6,10 @@
 
 struct svm_data {
     /*
-     * Store all samples (data sets) that participated in the calculation this time, 
-     * and their categories 
-     * *y: is an array what the lable to which the sample belongs 
-     * **data: an 2D array where the content is the pointer
+     * Store all samples (data sets) that participated in the calculation this time, and their categories 
+     * @params n_samples : 记录样本总数
+     * @params *y: an array containing the lable to which the sample belongs 样本所属类别(标签，如+1，-1)的数组
+     * @params **data: an 2D array where the content is the pointer, 存储样本内容除标签外的信息
      * 这样的数据结构有一个直接的好处，可以用x[i][j]来访问其中的某一元素
      */
 
