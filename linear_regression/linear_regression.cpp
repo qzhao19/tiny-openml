4cc1d239d721561c4f5abe58c32d2afa72c8c865
qzhao19 <qi.zhao@outlook.fr>
2021-06-30 20:37:36 +0800

    Create linear_regression.cpp

---
+++ linear_regression/linear_regression.cpp
@@ -0,0 +1,16 @@
+
+#include "linear_regression.hpp"
+using namespace regression;
+
+
+
+LinearRegression::LinearRegression(arma::mat &X, arma::rowvec &y, const double lambda, bool intercept) {
+    
+}
+
+
+
+
+
+
+
