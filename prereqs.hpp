bb0723e553622d290fb0db568c7589ff770ff743
qzhao19 <qi.zhao@outlook.fr>
2021-07-10 23:39:49 +0800

    create prereqs.cpp

---
+++ prereqs.hpp
@@ -0,0 +1,18 @@
+#ifndef PREREQS_HPP
+#define PREREQS_HPP
+
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+
+
+
+
+#endif
