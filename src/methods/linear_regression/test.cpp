353c6fc008b70272c9512d16d40117b8a6d5fdb0
qzhao19 <qi.zhao@outlook.fr>
2021-07-23 14:44:31 +0800

    Update and rename main.cpp to test.cpp

--- src/methods/linear_regression/test.cpp
+++ src/methods/linear_regression/test.cpp
@@ -1,5 +1,4 @@
 #include "linear_regression.hpp"
-#include <iostream>
 
 using namespace regression;
 
@@ -10,26 +9,24 @@ int main() {
 
     arma::vec y_train(50, arma::fill::randu);
 
-
     LinearRegression lr;
 
     lr.fit(X_train, y_train);
 
-    arma::vec theta = lr.get_theta();
-
-    std::cout <<"******************" << std::endl;
-    std::cout << theta << std::endl;
-    std::cout << theta.n_rows << std::endl;
-
-
     arma::mat X_test(10, 6, arma::fill::randu);
+    arma::vec y_test(10, arma::fill::randu);
 
-    arma::vec y_pred(10);
 
-    lr.predict(X_test, y_pred);
+    arma::vec y_pred;
+    y_pred = lr.predict(X_test);
 
     std::cout <<"******************" << std::endl;
     std::cout << y_pred << std::endl;
+
+    double score = lr.score(y_test, y_pred);
+
+    std::cout <<"******************" << std::endl;
+    std::cout << score << std::endl;
 
     return 0;
 
