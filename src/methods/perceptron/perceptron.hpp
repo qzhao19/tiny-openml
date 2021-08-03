b2ee335de196071103731170c3b43711501d717c
qzhao19 <qi.zhao@outlook.fr>
2021-08-03 23:40:14 +0800

    Update perceptron.hpp

--- src/methods/perceptron/perceptron.hpp
+++ src/methods/perceptron/perceptron.hpp
@@ -1,38 +1,46 @@
 #ifndef METHOD_PERCEPTRON_PERCEPTRON_HPP
 #define METHOD_PERCEPTRON_PERCEPTRON_HPP
+#include "../../core.hpp"
 #include "../../prereqs.hpp"
-
+#include "weight_initializer/zero_initializer.hpp"
 
 
 namespace perceptron {
 
+template<typename WeightInitializer>
 class Perceptron {
 
 public:
-    Perceptron(const double alpha_, 
-               const double tol_, 
-               const std::size_t max_iter_): 
-        alpha(alpha_), 
-        tol(tol_), 
-        max_iter(max_iter_) {};
-
-
-    Perceptron(): alpha(0.01), 
-                  tol(1e-5), 
-                  max_iter(10000) {};
+    Perceptron(): initializer("zeros"), 
+        shuffle(true),
+        alpha(0.01), 
+        tol(1e-5), 
+        max_iter(10000) {};
     
-
-    
-
+    Perceptron(const std::string initializer_,
+        const bool shuffle_,
+        const double alpha_, 
+        const double tol_, 
+        const std::size_t max_iter_): initializer(initializer_), 
+            shuffle(shuffle_),
+            alpha(alpha_), 
+            tol(tol_), 
+            max_iter(max_iter_) {};
 
 protected:
-    void fit(const arma::mat &X, const arma::vec &y);
+    void fit(const arma::mat &X, 
+             const arma::vec &y) const;
 
+    double sign(const arma::vec& x, 
+                const arma::vec& w, 
+                const double b) const;
 
 private:
-    arma::mat weights;
-    arma::vec biases;
+    arma::vec weights;
+    double bias;
 
+    std::string initializer;
+    bool shuffle;
     double alpha;
     double tol;
     std::size_t max_iter;
