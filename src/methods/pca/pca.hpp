1e1483d2446951fa9719fd4649494662195e5d76
qzhao19 <qi.zhao@outlook.fr>
2021-09-03 16:38:38 +0800

    Update pca.hpp

--- src/methods/pca/pca.hpp
+++ src/methods/pca/pca.hpp
@@ -1,1 +1,41 @@
+#ifndef METHOD_PCA_PCA_HPP
+#define METHOD_PCA_PCA_HPP
+#include "../../prereqs.hpp"
+#include "../../core.hpp"
 
+namespace pca{
+
+class PCA {
+public:
+
+    /**
+     * 
+    */
+    PCA(): solver("exact_svd"), 
+        n_components(2) {};
+
+
+    PCA(std::string solver_, 
+        std::size_t n_components_): 
+            solver(solver_), 
+            n_components(n_components_) {};
+    
+
+
+protected:
+    
+
+
+
+private:
+
+    arma::mat components;
+
+    std::string solver;
+
+    std::size_t n_components;
+    
+};
+
+};
+#endif
