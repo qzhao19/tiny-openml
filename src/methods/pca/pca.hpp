b25ea614f54fa196100e5695a042eb2d4b5dbc92
qzhao19 <qi.zhao@outlook.fr>
2021-09-13 22:21:01 +0800

    Update pca.hpp

--- src/methods/pca/pca.hpp
+++ src/methods/pca/pca.hpp
@@ -12,30 +12,41 @@ public:
      * 
     */
     PCA(): solver("exact_svd"), 
-        n_components(2) {};
+        n_components(2), 
+        scale(false) {};
 
 
     PCA(std::string solver_, 
-        std::size_t n_components_): 
+        std::size_t n_components_, 
+        bool scale_): 
             solver(solver_), 
-            n_components(n_components_) {};
+            n_components(n_components_), 
+            scale(scale_) {};
     
-
+    ~PCA() {};
 
 protected:
-    
+    /**
+     * 
+    */
+    template<typename DecompositionPolicy>
+    const arma::mat train(const arma::mat& X, 
+        DecompositionPolicy& decomposition_policy) const;
 
+    /**
+     * Scaling the data is when we reduce the variance of each dimension to 1.
+    */
+    void scale_data(arma::mat& X);
 
 
 private:
-
     arma::mat components;
 
     std::string solver;
 
     std::size_t n_components;
-    
-};
 
+    bool scale;
+};
 };
 #endif
