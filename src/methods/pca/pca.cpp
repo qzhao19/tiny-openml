77cfbaa4e46fc4ad3acc78bae40bbc3b948f7c42
qzhao19 <qi.zhao@outlook.fr>
2021-09-23 22:18:11 +0800

    Update pca.cpp

--- src/methods/pca/pca.cpp
+++ src/methods/pca/pca.cpp
@@ -1,50 +1,63 @@
-#include "pca.hpp"
+#ifndef METHOD_PCA_PCA_HPP
+#define METHOD_PCA_PCA_HPP
+#include "../../prereqs.hpp"
+#include "../../core.hpp"
 
-using namespace math;
-using namespace pca;
+#include "decomposition_policies/eig_method.hpp"
+#include "decomposition_policies/exact_svd_method.hpp"
 
-void PCA::scale_data(arma::mat& X) {
-    arma::vec scaled_vec = arma::stddev(X, 0, 1);
-    // if there are any zero, make them small
+namespace pca{
 
-    for (std::size_t i = 0; i < scaled_vec.n_elem; i++) {
-        if (scaled_vec[i] == 0) {
-            scaled_vec[i] = 1e-30;
-        }
-    }
-    X = arma::repmat(scaled_vec, 1, X.n_cols);
+class PCA {
+public:
+
+    /**
+     * 
+    */
+    PCA(): solver("exact_svd"), 
+        n_components(2), 
+        scale(false) {};
+
+
+    PCA(std::string solver_, 
+        std::size_t n_components_, 
+        bool scale_): 
+            solver(solver_), 
+            n_components(n_components_), 
+            scale(scale_) {};
     
-}
+    ~PCA() {};
 
-const arma::mat PCA::eig_train(const arma::mat& X) const {
-    arma::mat centered_X;
 
-    math::center(X, centered_X);
+    void fit(const arma::mat& X);
 
-    arma::vec eig_val;
-    arma::mat eig_vec;
+protected:
+    /**
+     * 
+    */
+    template<typename DecompositionPolicy>
+    const arma::mat svd_train(const arma::mat& X, 
+        DecompositionPolicy& decomposition_policy) const;
 
-    EigPolicy eig;
-    eig.Apply(centered_X, eig_val, eig_vec);
+    const arma::mat eig_train(const arma::mat& X) const;
 
-    return eig_vec.cols(n_components - 1, X.n_cols - 1);
+    /**
+     * Scaling the data is when we reduce the variance of each dimension to 1.
+    */
+    void scale_data(arma::mat& X);
 
-}
 
-template<typename DecompositionPolicy>
-const arma::mat PCA::svd_train(const arma::mat& X, 
-        DecompositionPolicy& decomposition_policy) const {
+private:
+
+    arma::mat components;
+
+    std::string solver;
+
+    std::size_t n_components;
+
+    bool scale;
     
-    arma::mat centered_X;
-    math::center(X, centered_X);
+};
 
-    arma::mat U;
-    arma::vec s;
-    arma::mat V;
-
-    decomposition_policy.Apply(centered_X, U, s, V);
-
-    
-}
-
-
+};
+#endif
