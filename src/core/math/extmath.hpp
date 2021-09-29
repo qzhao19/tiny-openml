2504185ad0be9d6822c4f42b5dd507a64624f896
qzhao19 <qi.zhao@outlook.fr>
2021-09-29 23:03:05 +0800

    Update extmath.hpp

--- src/core/math/extmath.hpp
+++ src/core/math/extmath.hpp
@@ -15,14 +15,39 @@ namespace math {
  * @param u_based_decision Bool If True, use the columns of u as the basis for sign flipping. 
  * Otherwise, use the rows of v. The choice of which variable to base the 
  * decision on is generally algorithm dependent.
+ * 
+ * @return u_adjusted, v_adjusted : arrays with the same dimensions as the input.
 */
-
 template<typename MatType>
-void center(MatType& U, MatType& V, bool u_based_decision = true) {
+void svd_flip(MatType& U, MatType& V, bool u_based_decision = true) {
 
     
 
 };
+
+
+/**
+ * Compute log(det(A)) for A symmetric.
+ * Equivalent to : arma::log(arma::det(X)) but more robust. 
+ * It returns -Inf if det(A) is non positive or is not defined.
+ * 
+ * @param X Input matrix 
+*/
+template<typename MatType>
+double logdet(MatType& X) {
+
+    double val;
+    double sign;
+
+    arma::log_det(val, sign, X);
+
+    if (sign < 0) {
+        return -arma::datum::inf;
+    }
+    return val;
+};
+
+
 
 
 };
