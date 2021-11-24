303ca6d2c3cf5811687d8e2771d62fd6b53e265f
qzhao19 <qi.zhao@outlook.fr>
2021-11-24 20:36:36 +0800

    Update naive_bayes.cpp

--- src/methods/naive_bayes/naive_bayes.cpp
+++ src/methods/naive_bayes/naive_bayes.cpp
@@ -7,20 +7,16 @@ using namespace naive_bayes;
 void NaiveBayes::get_log_class_prior_prob(const arma::vec& y) {
 
     int n_samples = y.n_rows;
-    std::unordered_map<double, int> counter;
-
-    for (std::size_t i = 0; i < n_samples; i++) {
-        counter[y[i]]++;
-    }
-
     std::vector<double> class_prior_prob;
 
-    n_classes = 0;
-    for (auto &cnt : counter) {
-        label_map[cnt.first] = n_classes;
-        n_classes++;
+    for (std::size_t i = 0; i < n_samples; i++) {
+        label_map[y[i]]++;
+    }
 
-        class_prior_prob.push_back(double(cnt.second / n_samples));
+    n_classes = 0;
+    for (auto &label : label_map) {
+        n_classes++;
+        class_prior_prob.push_back(label.second / n_samples);
     }
 
     log_class_prior_prob = arma::conv_to<arma::vec>::from(class_prior_prob);
@@ -32,20 +28,19 @@ void NaiveBayes::get_log_class_prior_prob(const arma::vec& y) {
 void NaiveBayes::update_mean_variance(const arma::mat& X, 
     const arma::vec& y) {
     
-    std::vector<double> new_y;
-    for (auto &y_i : y) {
-        new_y.push_back(label_map[y_i]);
+    int n_features = X.n_cols;
+    arma::mat X_y = arma::join_rows(X, y);
+
+    std::size_t i = 0;
+    for (auto &label : label_map) {
+        arma::mat partial_X_y= X.rows(arma::find(X_y.col(n_features) == label.first));
+        arma::rowvec partial_means = arma::mean(partial_X_y, 0);
+        arma::rowvec partial_vars = arma::var(partial_X_y, 0, 0);
+
+        means.insert_rows(i, partial_means);
+        vars.insert_rows(i, partial_vars);
+        i++;
     }
-
-    std::vector<double> means_, vars_;
-    for (std::size_t i = 0; i < n_classes; i++) {
-        means_.push_back(i);
-        vars_.push_back(i);
-    }
-
-    means = arma::conv_to<arma::vec>::from(means_);
-    vars = arma::conv_to<arma::vec>::from(vars_);
-
 }
 
 
