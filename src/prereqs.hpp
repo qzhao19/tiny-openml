2bf0a64867b40584e7a55e8a8534279cc9116882
qzhao19 <qi.zhao@outlook.fr>
2021-12-01 16:57:31 +0800

    Update prereqs.hpp

--- src/prereqs.hpp
+++ src/prereqs.hpp
@@ -2,20 +2,23 @@
 #define PREREQS_HPP
 
 #include <algorithm>
-#include <armadillo>
 #include <cctype>
 #include <cfloat>
 #include <climits>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
-#include <ensmallen.hpp>
 #include <iostream>
+#include <map>
 #include <random>
 #include <string>
 #include <stdexcept>
 #include <tuple>
 #include <utility>
+#include <unordered_map>
+#include <vector>
 
+#include <armadillo>
+#include <ensmallen.hpp>
 
 #endif
