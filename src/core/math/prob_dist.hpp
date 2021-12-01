1b19b5ec3b27e1f97da8b7398a277c94d4b7607a
qzhao19 <qi.zhao@outlook.fr>
2021-12-01 15:08:32 +0800

    Update prob_dist.hpp

--- src/core/math/prob_dist.hpp
+++ src/core/math/prob_dist.hpp
@@ -6,19 +6,21 @@ namespace math {
 
 /**
  * gaussian probability distribution
+ * -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * pi * v))
  * 
  * @param x DataType, input value
  * @param u DataType, mean
  * @param v DataType, variance 
- * @param retval DataType, return gaussiance value 
 */
 template<typename DataType>
-void gaussian_fn(const DataType& x, 
+double gaussian_fn(const DataType& x, 
     const DataType& u, 
-    const DataType& v, 
-    DataType& retval) {
+    const DataType& v) {
     
-    retval = std::pow(-(x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * arma::datum::pi * v));
+    // retval = -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * arma::datum::pi * v));
+    double retval = -1.0 * std::pow((x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * arma::datum::pi * v));
+
+    return retval;
 };
 
 
