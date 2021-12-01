ea38aad77b6935ffb9bd9cebb78bb06425b7b617
qzhao19 <qi.zhao@outlook.fr>
2021-12-01 16:57:55 +0800

    Update core.hpp

--- src/core.hpp
+++ src/core.hpp
@@ -1,13 +1,14 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
+#include "../src/core/data/load_data.hpp"
+
 #include "../src/core/math/shuffle_data.hpp"
 #include "../src/core/math/lin_alg.hpp"
 #include "../src/core/math/extmath.hpp"
 #include "../src/core/math/prob_dist.hpp"
 
-
-#include "../src/core/data/load_data.hpp"
+#include "../src/core/utility/utils.hpp"
 
 
 #endif
