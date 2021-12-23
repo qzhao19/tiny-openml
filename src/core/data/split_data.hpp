1152f67aea27f347a1c3867b36f8cb6076fbaba6
qzhao19 <qi.zhao@outlook.fr>
2021-12-24 00:16:24 +0800

    Create split_data.hpp

---
+++ src/core/data/split_data.hpp
@@ -0,0 +1,35 @@
+#ifndef CORE_DATA_SPLIT_DATA_HPP
+#define CORE_DATA_SPLIT_DATA_HPP
+#include "../../prereqs.hpp"
+#include "../../core.hpp"
+
+template<typename MatType, 
+         typename VecType>
+std::tuple<MatType, MatType, VecType, VecType> train_test_split(const MatType& X, 
+        const VecType& y,
+        double train_size = 0.75, 
+        bool shuffle = false) {
+    
+    std::size_t n_samples = X.n_rows;
+
+    arma::mat X_ = X;
+    arma::vec y_ = y;
+
+    if (shuffle) {
+        shuffle_data(X, y, X_, y_);
+    }
+
+    int n_train_samples = n_samples * train_size;
+    int n_test_samples = n_samples - n_train_samples;
+
+    arma::mat X_train = X_.head_rows(n_train_samples);
+    arma::mat X_test = X_.tail_rows(n_test_samples);
+    arma::vec y_train = y_.head_rows(n_train_samples);
+    arma::vec y_test = y_.tail_rows(n_test_samples);
+
+    return std::make_tuple(X_train, X_test, y_train, y_test);
+
+}
+
+
+#endif
