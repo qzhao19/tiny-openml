263f7cf645efdb30d542c91854c2231d7daa0d25
qzhao19 <qi.zhao@outlook.fr>
2021-12-30 22:32:29 +0800

    Update exact_svd_method.hpp

--- src/methods/pca/decomposition_policies/exact_svd_method.hpp
+++ src/methods/pca/decomposition_policies/exact_svd_method.hpp
@@ -9,38 +9,27 @@ public:
     /**
      * Implementaion of exact svd method using API that provided by arma
      * 
-     * @param X Data matrix of shape (n_samples, n_faetures)
+     * @param X Data matrix of shape (n_samples, n_features)
      * @param X_centered Centered data matrix, data minus the mean of X
-     * @param X_transformed Matrix to put result of pca
      * @param U Matrix to put eigen vector
      * @param s Vector to put eigen values
-     * @param rank Rank of the decomposition
+     * @param Vt Matrix store the right singualr values
     */
     void Apply(const arma::mat& X, 
-        const arma::mat& X_centered, 
-        arma::mat& X_transformed, 
         arma::mat& U, 
         arma::vec& s, 
-        const std::size_t rank) {
-        
-        // this matrix store the right singualr values
-        arma::mat V;
-
+        arma::mat Vt) {
+                
         // if ncols are much larger than nrows, need to use economical svd
         if (X.n_cols > X.n_rows) {
             // economical singular value decomposition and compute only the left
             // singular vectors
-            arma::svd_econ(U, s, V, X_centered, "left");
+            arma::svd_econ(U, s, Vt, X, "left");
         }
         else {
-             arma::svd(U, s, V, X_centered);
+             arma::svd(U, s, Vt, X);
         }   
-
         s %= s / (X.n_cols - 1);
-
-        // project the samples into the principle 
-        X_transformed = arma::trans(s) * X_transformed;
-
     };
 
 };
