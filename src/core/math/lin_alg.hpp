d81c5a2d916040cb3e9a98e7c295667720c0b401
qzhao19 <qi.zhao@outlook.fr>
2021-12-31 23:26:58 +0800

    Update lin_alg.hpp

--- src/core/math/lin_alg.hpp
+++ src/core/math/lin_alg.hpp
@@ -3,21 +3,46 @@
 #include "../../prereqs.hpp"
 
 namespace math {
+/**
+ * Eigen vector decomposition
+ * @param X ndarray of shape (n_samples, n_features),
+ *          matrices for which the eigenvalues and right 
+ *          eigenvectors will be computed 
+ * 
+ * @return std::tuple(eigenvector, eigenvalue), the eigenvalues and 
+ *         the eigenvectors
+*/
+template<typename MatType, typename VecType>
+std::tuple<MatType, VecType> eig(const MatType& X) {
+    VecType eig_val;
+    MatType eig_vec;
+    arma::eig_gen(eig_val, eig_vec, X);
+
+    return std::make_tuple(eig_vec, eig_val);
+};
 
 /**
- * Calculate centered matrix, where cerntering is done by substracting the mean 
- * over the colnum from each col of the matrix.
- * 
- * @param X Input matrix
- * @param X_centered Matrix to write centered output
+ * Singular Value Decomposition
+ * @param X ndarray of shape (n_samples, n_features),
+ *          A real or complex array
+ * @param full_matrices, bool, default = false
+ *        If True (default), u and vh have the shapes (..., M, M) and (..., N, N). 
+ *        Otherwise, the shapes are (..., M, K) and (..., K, N), respectively, where K = min(M, N).
 */
-template<typename MatType>
-void center(const MatType& X, MatType& X_centered) {
+template<typename MatType, typename VecType>
+std::tuple<MatType, VecType, MatType> svd(const MatType& X, 
+    bool full_matrices = false) {
+    MatType U;
+    VecType s; 
+    MatType Vt;
 
-    arma::rowvec x_mean = arma::mean(X, 0);
-
-    X_centered = X - arma::repmat(x_mean, X.n_rows, 1);
-
+    if (full_matrices) {
+        arma::svd(U, s, Vt, X);
+    }
+    else {
+        arma::svd_econ(U, s, Vt, X);
+    }
+    return std::make_tuple(U, s, Vt)
 };
 
 
