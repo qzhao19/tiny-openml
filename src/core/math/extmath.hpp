cae2540b9527826950e2da88250510653f2e0853
qzhao19 <qi.zhao@outlook.fr>
2021-12-31 23:27:46 +0800

    Update extmath.hpp

--- src/core/math/extmath.hpp
+++ src/core/math/extmath.hpp
@@ -19,10 +19,40 @@ namespace math {
  * @return u_adjusted, v_adjusted : arrays with the same dimensions as the input.
 */
 template<typename MatType>
-void svd_flip(MatType& U, MatType& V, bool u_based_decision = true) {
-    arma::mat svd;
-};
+std::tuple<MatType, MatType> svd_flip(const MatType& U, 
+    const MatType &Vt, 
+    bool u_based_decision = true) {
+    
+    MatType U_, Vt_;
+    if (u_based_decision) {
+        arma::urowvec  max_abs_idx = arma::index_max(arma::abs(U), 0); 
+        arma::rowvec max_abs_cols(max_abs_idx.n_elem);
+        for(std::size_t j = 0; j < max_abs_idx.n_elem; j++) {
+            std::size_t i = max_abs_idx(j);
+            max_abs_cols(j) = U(i, j);
+        }
 
+        arma::rowvec signs = arma::sign(max_abs_cols);
+        U_ = U % arma::repmat(signs, U.n_rows, 1);
+        arma::vec signs_ = arma::conv_to<arma::vec>::from(signs);
+        Vt_ = Vt % arma::repmat(signs_, 1, Vt.n_cols);
+    }
+    else {
+        arma::uvec max_abs_idx = arma::index_max(arma::abs(Vt), 1); 
+        arma::rowvec max_abs_rows(max_abs_idx.n_elem);
+
+        for (std::size_t i = 0; i < max_abs_rows.n_elem; i++) {
+            std::size_t j = max_abs_idx(i);
+            max_abs_rows(i) = Vt(i, j);
+        }
+        arma::rowvec signs = arma::sign(max_abs_rows);
+        U_ = U % arma::repmat(signs, U.n_rows, 1);
+        arma::vec signs_ = arma::conv_to<arma::vec>::from(signs);
+        Vt_ = Vt % arma::repmat(signs_, 1, Vt.n_cols);
+    }
+
+    return std::make_tuple(U_, Vt_);
+};
 
 /**
  * Compute log(det(A)) for A symmetric.
@@ -35,6 +65,7 @@ template<typename MatType>
 double logdet(const MatType& X) {
     double val;
     double sign;
+
     arma::log_det(val, sign, X);
 
     if (!(sign > 0)) {
@@ -53,6 +84,19 @@ Type flatten(const std::vector<Type>& v) {
         return dest;
     });
 }
+
+/**
+ * Calculate centered matrix, where cerntering is done by substracting the mean 
+ * over the colnum from each col of the matrix.
+ * 
+ * @param X Input matrix
+ * @param X_centered Matrix to write centered output
+*/
+template<typename MatType>
+void center(const MatType& X, MatType& X_centered) {
+    arma::rowvec x_mean = arma::mean(X, 0);
+    X_centered = X - arma::repmat(x_mean, X.n_rows, 1);
+};
 
 };
 #endif
