f67cf06a0a0beb739f62e50fe8fafd2b955837a9
qzhao19 <qi.zhao@outlook.fr>
2022-01-04 22:10:22 +0800

    Update sgd_regressor.hpp

--- src/methods/sgd_regressor/sgd_regressor.hpp
+++ src/methods/sgd_regressor/sgd_regressor.hpp
@@ -2,33 +2,58 @@
 #define METHOD_SGD_REGRESSION_SGD_REGRESSION_HPP
 #include "../../prereqs.hpp"
 #include "../../core.hpp"
+using namespace loss;
+using namespace math;
 
 namespace regression {
 
 class SGDRegressor {
 
-    SGDRegressor(): max_iters(10000), 
-        alpha(0.0001),
-        penalty("l2"),
-        loss("squared_error"),
-        learning_rate("optimal"), 
+public:
+    SGDRegressor(): loss("squared_error"),
+        penalty("l2"), 
+        learning_rate("optimal"),
+        lambda(0.0001),
+        l1_ratio(0.15),
+        batch_size(32),
+        max_iter(100), 
         shuffle(true) {};
 
+    SGDRegressor(const std::string loss_, 
+        const std::string penalty_ = "l2", 
+        const std::string learning_rate_ = "optimal",
+        const double lambda_ = 0.0001,
+        const double l1_ratio_ = 0.15,
+        const std::size_t batch_size_ = 32, 
+        const std::size_t max_iter_ = 100, 
+        const bool shuffle_ = true): 
+            loss(loss_), 
+            penalty(penalty_), 
+            learning_rate(learning_rate_), 
+            lambda(lambda_), 
+            l1_ratio(l1_ratio_),
+            batch_size(batch_size_), 
+            max_iter(max_iter_), 
+            shuffle(shuffle_){}
+    
     ~SGDRegressor() {};
 
-
-    template<typename LossFunction>
     void fit(const arma::mat& X, const arma::vec& y);
 
-
-
+protected:
+    template<class LossFunctionType>
+    void fit(const arma::mat& X, 
+        const arma::vec& y, 
+        LossFunctionType& loss_fn_type);
 
 private:
-    arma::vec theta;
+    arma::vec W;
 
-    int max_iters;
+    std::size_t batch_size;
 
-    double alpha;
+    std::size_t max_iter;
+
+    double lambda;
 
     double l1_ratio;
 
