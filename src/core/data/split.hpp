f45c5162b43f51a8d5bc2c7124590b0a69f30603
qzhao19 <qi.zhao@outlook.fr>
2022-01-10 23:19:37 +0800

    Update and rename split_data.hpp to split.hpp

--- src/core/data/split.hpp
+++ src/core/data/split.hpp
@@ -1,10 +1,10 @@
-#ifndef CORE_DATA_SPLIT_DATA_HPP
-#define CORE_DATA_SPLIT_DATA_HPP
+#ifndef CORE_DATA_SPLIT_HPP
+#define CORE_DATA_SPLIT_HPP
 #include "../../prereqs.hpp"
 #include "../../core.hpp"
+using namespace openml;
 
-using namespace math;
-
+namespace openml {
 namespace data {
 
 /**
@@ -22,12 +22,12 @@ template<typename MatType, typename VecType>
 std::tuple<MatType, MatType, VecType, VecType> train_test_split(const MatType& X, 
         const VecType& y,
         double train_size = 0.75, 
-        bool shuffle = false) {
+        bool shuffle = true) {
     
-    std::size_t n_samples = X.n_rows;
+    std::size_t n_samples = X.rows(), n_features = X.cols();
 
-    arma::mat X_ = X;
-    arma::vec y_ = y;
+    MatType X_ = X;
+    VecType y_ = y;
 
     if (shuffle) {
         math::shuffle_data(X, y, X_, y_);
@@ -36,14 +36,15 @@ std::tuple<MatType, MatType, VecType, VecType> train_test_split(const MatType& X
     int n_train_samples = n_samples * train_size;
     int n_test_samples = n_samples - n_train_samples;
 
-    arma::mat X_train = X_.head_rows(n_train_samples);
-    arma::mat X_test = X_.tail_rows(n_test_samples);
-    arma::vec y_train = y_.head_rows(n_train_samples);
-    arma::vec y_test = y_.tail_rows(n_test_samples);
+    MatType X_train = X_.topRows(n_train_samples);
+    MatType X_test = X_.bottomRows(n_test_samples);
+    VecType y_train = y_.topRows(n_train_samples);
+    VecType y_test = y_.bottomRows(n_test_samples);
 
     return std::make_tuple(X_train, X_test, y_train, y_test);
+};
 
-}
+} // namespace split
+} // namespace openml
 
-}
 #endif
