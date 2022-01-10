b4039e6c14d7b2b6534a4df3f241ce7e8638d9df
qzhao19 <qi.zhao@outlook.fr>
2022-01-10 23:19:01 +0800

    Update and rename load_data.hpp to load.hpp

--- src/core/data/load.hpp
+++ src/core/data/load.hpp
@@ -1,7 +1,6 @@
-#ifndef CORE_DATA_LOAD_DATA_HPP
-#define CORE_DATA_LOAD_DATA_HPP
+#ifndef CORE_DATA_LOAD_HPP
+#define CORE_DATA_LOAD_HPP
 #include "../../prereqs.hpp"
-#include "../../core.hpp"
 
 namespace openml {
 namespace data {
@@ -9,39 +8,39 @@ namespace data {
 /**
  * load dataset from a txt file
  * @param fp String the given filepath 
- * @param data 2d-array of shape (n_samples, n_features) the output matrix 
- * 
+ * @param X 2d-array of shape (n_samples, n_features) the output matrix 
+ * @param y 2d-array of shape (n_samples, 1) the output label matrix 
 */
 template<typename MatType, 
+    typename VecType,
     typename DataType = typename MatType::value_type>
 void loadtxt(const std::string &fp, 
-    MatType& data) {
-
-    using Matrix = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
-    using Vector = Eigen::Vector<DataType, Eigen::Dynamic>;
+    MatType& X, 
+    VecType& y) {
 
     std::vector<std::vector<DataType>> stdmat;
-
     std::ifstream file_in(fp);
     for (std::string line; std::getline(file_in, line); ) {
-        std::stringstream ss(line);
+        std::stringstream str_stream(line);
         std::vector<DataType> row;
 
-        for (DataType elem; ss >> elem; ) {
+        for (DataType elem; str_stream >> elem; ) {
             row.push_back(elem);
         }
         stdmat.push_back(row);
     }
 
+    // conert std 2D vector into eigen matrix
     std::size_t n_rows = stdmat.size(), n_cols = stdmat[0].size();
-    Matrix mat(n_rows, n_cols);
+    MatType mat(n_rows, n_cols);
     for (std::size_t i = 0; i < n_rows; i++) {
-        mat.row(i) = Vector::Map(&stdmat[i][0], n_cols);
+        mat.row(i) = VecType::Map(&stdmat[i][0], n_cols);
     }
-    data = mat;
+    X = mat.leftCols(n_cols - 1);
+    y = mat.rightCols(1);
     
 };
 
 }
 }
-#endif
+#endif /*CORE_DATA_LOAD_HPP*/
