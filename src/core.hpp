71a52081691ab25ddc89dc631959a190c2047552
qzhao19 <qi.zhao@outlook.fr>
2022-01-11 23:07:12 +0800

    Update core.hpp

--- src/core.hpp
+++ src/core.hpp
@@ -1,20 +1,20 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
-#include "../src/core/math/shuffle_data.hpp"
+#include "../src/core/math/shuffle.hpp"
 #include "../src/core/math/linalg.hpp"
 #include "../src/core/math/extmath.hpp"
-#include "../src/core/math/prob_dist.hpp"
+#include "../src/core/math/dists.hpp"
 
-#include "../src/core/utility/utils.hpp"
+#include "../src/core/utils.hpp"
 
-#include "../src/core/data/load_data.hpp"
-#include "../src/core/data/split_data.hpp"
+#include "../src/core/data/load.hpp"
+#include "../src/core/data/split.hpp"
 
-#include "../src/core/loss/log_loss.hpp"
-#include "../src/core/loss/mean_squared_error.hpp"
+// #include "../src/core/loss/log_loss.hpp"
+// #include "../src/core/loss/mean_squared_error.hpp"
 
-#include "../src/core/initializer/zero_initializer.hpp"
-#include "../src/core/initializer/rand_initializer.hpp"
+// #include "../src/core/initializer/zero_initializer.hpp"
+// #include "../src/core/initializer/rand_initializer.hpp"
 
-#endif
+#endif /*CORE_HPP*/
