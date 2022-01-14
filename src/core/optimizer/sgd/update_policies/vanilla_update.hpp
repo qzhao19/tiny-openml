4dd15d8489e1a71ee8550c6513f7fd08bc545bbf
qzhao19 <qi.zhao@outlook.fr>
2022-01-14 22:24:25 +0800

    Create vanilla_update.hpp

---
+++ src/core/optimizer/sgd/update_policies/vanilla_update.hpp
@@ -0,0 +1,18 @@
+#ifndef CORE_OPTIMIZER_SGD_UPDATE_POLICIES_VANILLA_UPDATE_HPP
+#define CORE_OPTIMIZER_SGD_UPDATE_POLICIES_VANILLA_UPDATE_HPP
+
+namespace openml {
+namespace optimizer {
+
+template<typename DataType>
+class VanillaUpdate {
+
+    
+
+
+
+};
+
+}
+}
+#endif /*CORE_OPTIMIZER_SGD_UPDATE_POLICIES_VANILLA_UPDATE_HPP*/
