586013492632fbf481b0ae05378f27e37df82d0d
qzhao19 <qi.zhao@outlook.fr>
2022-01-17 00:11:34 +0800

    Update log_loss.hpp

--- src/core/loss/log_loss.hpp
+++ src/core/loss/log_loss.hpp
@@ -13,63 +13,47 @@ private:
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
-    MatType X;
-    VecType y;
     double lambda;
     std::string penalty;
-    
-    std::size_t num_samples;
-    std::size_t num_features;
     
 public:
     /**
      * create the log loss constructor
      * 
-     * @param X ndarray of shape (num_samples, num_features), input data matrix
-     * @param y ndarray of shape (num_samples,) target vector relative to X.
      * @param lambda double, regularization strength; must be a positive float. 
      * @param penalty string, specify the norm of the penalty {‘l2’, ‘none’}, default='None'
      * 
     */
-    LogLoss(const MatType& X_, 
-        const VecType& y_, 
-        const DataType lambda_ = static_cast<DataType>(0), 
-        const std::string penalty_ = "None"): X(X_), y(y_), 
-            lambda(lambda_), 
+    LogLoss(const DataType lambda_ = static_cast<DataType>(0), 
+        const std::string penalty_ = "None"): lambda(lambda_), 
             penalty(penalty_) {
-                std::size_t num_samples = X.rows();
-                std::size_t num_features = X.cols();
+                // std::size_t num_samples = X_.rows();
+                // std::size_t num_features = X_.cols();
             };
 
     ~LogLoss() {};
 
-    void shuffle() {
-        math::shuffle_data(X, y, X, y);
-    };
-
     /**
-     * calculate the log loos function value
-     * L(w) = sum{y_i * Wt * X_i - log[1 + exp(Wt * X_i)]}
+     * evaluate the gradient of the logistic regression log-likelihood function
+     * with the given parameters.
+     *          L(w) = sum{y_i * Wt * X_i - log[1 + exp(Wt * X_i)]}
      * 
+     * @param X ndarray of shape (num_samples, num_features), the matrix of input data
+     * @param y ndarray of shape (num_samples) 
      * @param W ndarray of shape (num_features, 1) coefficient of the features 
-     * @param begin int, the begin index of one batch data
-     * @param batch_size int desired batch size for mini batches.
     */
-    double evaluate(const VecType& W, 
-        const std::size_t begin,
-        const std::size_t batch_size) const {
+    double evaluate(const MatType& X, 
+        const VecType& y, 
+        const VecType& W) const {
         
-        // get one batch data 
-        MatType X_batch(batch_size, num_features);
-        VecType y_batch(batch_size);
-        X_batch = X.middleRows(begin, batch_size);
-        y_batch = y.middleRows(begin, batch_size);
+        std::size_t num_samples = X.rows();
+        std::size_t num_features = X.cols();
 
         DataType error;
         DataType penalty_val;
-        for (std::size_t i = 0; i < batch_size; i++) {
-            double x_w = X_batch.row(i) * W;
-            error += ((y_batch(i, 0) * x_w) - 
+        for (std::size_t i = 0; i < num_samples; i++) {
+            double x_w = X.row(i) * W;
+            error += ((y(i, 0) * x_w) - 
                     std::log(1.0 + std::exp(x_w)));
         }
         // regularization term
@@ -77,46 +61,46 @@ public:
             penalty_val = static_cast<DataType>(0);
         }
         else if (penalty == "l2") {
-            penalty_val = (W.transpose() * W);
+            penalty_val = W.transpose() * W;
             // penalty_val = penalty_val * lambda;
         }
-        penalty_val = penalty_val / (static_cast<double>(batch_size));
-        error = error / (-static_cast<double>(batch_size));
+        penalty_val = (penalty_val * lambda) / (static_cast<double>(num_samples));
+        error = error / (-static_cast<double>(num_samples));
         return error + penalty_val;
-    }
+    };
 
-    void gradient(const VecType& W, 
-        VecType& grad,
-        const std::size_t begin,
-        const std::size_t batch_size) {
+    /**
+     * Evaluate the gradient of logistic regression log-likelihood function with the given 
+     * parameters using the given batch size from the given point index
+     * 
+     *      grad = (1 / m) * sum(sigmoid(x) - y) + (lambda / m) * W
+    */
+    VecType gradient(const MatType& X, 
+        const VecType& y,
+        const VecType& W) const{
         
-        MatType X_batch(batch_size, num_features);
-        VecType y_batch(batch_size);
-        X_batch = X.middleRows(begin, batch_size);
-        y_batch = y.middleRows(begin, batch_size);
+        std::size_t num_samples = X.rows();
+        std::size_t num_features = X.cols();
 
-        VecType X_w(batch_size); 
-        VecType y_hat(batch_size); 
-        X_w = X_batch * W;
+        VecType X_w(num_samples); 
+        VecType y_hat(num_samples); 
+        X_w = X * W;
         y_hat = math::sigmoid<VecType>(X_w);
-
+        
+        VecType grad(num_features);
+        // grad = (X.transpose() * (y_hat - y) + lambda * W) / num_samples;
         if (penalty == "None") {
-            grad = (X_batch.transpose() * (y_hat - y_batch) + lambda * W) / batch_size;
+            grad = (X.transpose() * (y_hat - y) + lambda * W) / num_samples;
         }
         else if (penalty == "l2") {
-            grad = (X_batch.transpose() * (y_hat - y_batch)) / batch_size; + 
+            grad = (X.transpose() * (y_hat - y)) / num_samples + 
                 static_cast<DataType>(2) * lambda * W;
         }
-    };
 
-    const std::size_t get_num_samples() {
-        return num_samples;
-    };
-
-    const std::size_t get_num_features() {
-        return num_features;
+        return grad;
     };
 };
+
 
 }
 }
