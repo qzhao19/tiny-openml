7c9d8eb4a9971e93519d89cab265ccd24ed850de
qzhao19 <qi.zhao@outlook.fr>
2022-01-17 23:01:17 +0800

    Update vanilla_update.hpp

--- src/core/optimizer/sgd/update_policies/vanilla_update.hpp
+++ src/core/optimizer/sgd/update_policies/vanilla_update.hpp
@@ -1,16 +1,25 @@
 #ifndef CORE_OPTIMIZER_SGD_UPDATE_POLICIES_VANILLA_UPDATE_HPP
 #define CORE_OPTIMIZER_SGD_UPDATE_POLICIES_VANILLA_UPDATE_HPP
+#include "../../../../prereqs.hpp"
+#include "../../../../core.hpp"
 
 namespace openml {
 namespace optimizer {
 
 template<typename DataType>
 class VanillaUpdate {
+private:
+    using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
+    DataType alpha;
 
-    
+public:
+    VanillaUpdate(const DataType alpha_): alpha(alpha_) {};
+    ~VanillaUpdate() {}; 
 
-
-
+    void update(const VecType& W, const VecType& grad, VecType& updated_W) {
+        updated_W = W - alpha * grad;
+    };
 };
 
 }
