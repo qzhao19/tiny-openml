0e9e011a4f7b32a0d84a94cd13ad3316d63601c8
qzhao19 <qi.zhao@outlook.fr>
2022-02-12 23:13:54 +0800

    Update extmath.hpp

--- src/core/math/extmath.hpp
+++ src/core/math/extmath.hpp
@@ -14,14 +14,14 @@ namespace math {
 /**
  * compute the matrix sigmoid value
  *      s(z) = 1 / (1 + exp(-z))
- * @param mat ndarray of shape [num_rows, num_cols]
+ * @param x ndarray of shape [num_rows, num_cols]
  * @return sigmoid matrix 
 */
 template<typename MatType, 
     typename DataType = typename MatType::value_type>
-MatType sigmoid(const MatType& mat) {
+MatType sigmoid(const MatType& x) {
     return (static_cast<DataType>(1) / 
-        (static_cast<DataType>(1) + (-mat.array()).exp())).matrix();
+        (static_cast<DataType>(1) + (-x.array()).exp())).matrix();
 }
 
 /**
@@ -29,13 +29,13 @@ MatType sigmoid(const MatType& mat) {
  * return is a scalar, if input data is a matrtix, return 
  * is the covariamce matrix of ndarray.
  * 
- * @param mat input data of type vector or matrix 
+ * @param x input data of type vector or matrix 
  * @return scalar or 2darray
 */
 template<typename AnyType>
-AnyType var(const AnyType& mat) {
-    AnyType centered = mat.rowwise() - mat.colwise().mean();
-    AnyType cov = (centered.adjoint() * centered) / static_cast<double>(mat.rows() - 1);
+AnyType var(const AnyType& x) {
+    AnyType centered = x.rowwise() - x.colwise().mean();
+    AnyType cov = (centered.adjoint() * centered) / static_cast<double>(x.rows() - 1);
     return cov;
 };
 
@@ -43,13 +43,13 @@ AnyType var(const AnyType& mat) {
  * Calculate centered matrix, where cerntering is done by substracting the mean 
  * over the colnum from each col of the matrix.
  * 
- * @param mat Input matrix
+ * @param x Input matrix
  * @param center_mat Matrix to write centered output
 */
 template<typename MatType>
-void center(const MatType& mat, MatType& center_mat) {
-    std::size_t num_rows = mat.rows();
-    center_mat = repeat<MatType>(mat.colwise().mean(), num_rows, 0);
+void center(const MatType& x, MatType& center_x) {
+    std::size_t num_rows = x.rows();
+    center_x = repeat<MatType>(x.colwise().mean(), num_rows, 0);
 };
 
 /**
@@ -61,7 +61,6 @@ template<typename AnyType>
 AnyType abs(const AnyType& x) {
     return x.array().abs().matrix();
 }
-
 
 /**
  * Returns an element-wise indication of the sign of a number.
