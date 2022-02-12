2bd85702a5acb85d9aa89dde31ffd8974e7acc2f
qzhao19 <qi.zhao@outlook.fr>
2022-02-12 23:36:04 +0800

    Update linalg.hpp

--- src/core/math/linalg.hpp
+++ src/core/math/linalg.hpp
@@ -16,39 +16,39 @@ namespace math {
  *      {(M, M), (M, K)}, (K), {(N, N), (K, N)}
 */
 template<typename MatType, typename VecType>
-std::tuple<MatType, VecType, MatType> exact_svd(const MatType& mat, 
-    bool full_matrices = false) {
+std::tuple<MatType, VecType, MatType> exact_svd(const MatType& x, 
+    bool full_matrix = false) {
     MatType U;
     VecType s; 
     MatType Vt;
 
-    std::size_t num_features = mat.cols();
+    std::size_t num_features = x.cols();
     // control the switching size, default is 16
     // For small matrice (<16), it is thus preferable to directly use JacobiSVD. 
     // For larger matrice, BDCSVD
     if (num_features < 16) {
-        if (full_matrices) {
-            Eigen::JacobiSVD<Eigen::MatrixXd> svd(mat, Eigen::ComputeFullU | Eigen::ComputeFullV);
+        if (full_matrix) {
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd(x, Eigen::ComputeFullU | Eigen::ComputeFullV);
             U = svd.matrixU();
             s = svd.singularValues();
             Vt = svd.matrixV();
         }
         else {
-            Eigen::JacobiSVD<Eigen::MatrixXd> svd(mat, Eigen::ComputeThinU | Eigen::ComputeThinV);
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd(x, Eigen::ComputeThinU | Eigen::ComputeThinV);
             U = svd.matrixU();
             s = svd.singularValues();
             Vt = svd.matrixV();
         }
     }
     else {
-        if (full_matrices) {
-            Eigen::BDCSVD<Eigen::MatrixXd> svd(mat, Eigen::ComputeFullU | Eigen::ComputeFullV);
+        if (full_matrix) {
+            Eigen::BDCSVD<Eigen::MatrixXd> svd(x, Eigen::ComputeFullU | Eigen::ComputeFullV);
             U = svd.matrixU();
             s = svd.singularValues();
             Vt = svd.matrixV();
         } 
         else {
-            Eigen::BDCSVD<Eigen::MatrixXd> svd(mat, Eigen::ComputeThinU | Eigen::ComputeThinV);
+            Eigen::BDCSVD<Eigen::MatrixXd> svd(x, Eigen::ComputeThinU | Eigen::ComputeThinV);
             U = svd.matrixU();
             s = svd.singularValues();
             Vt = svd.matrixV();
@@ -57,6 +57,45 @@ std::tuple<MatType, VecType, MatType> exact_svd(const MatType& mat,
 
     return std::make_tuple(U, s, Vt);
 };
+
+/**
+ * Compute the (Moore-Penrose) pseudo-inverse of a matrix. 
+ * Calculate the generalized inverse of a matrix using its 
+ * singular-value decomposition (SVD) and including all 
+ * large singular values.
+ * 
+ * @param x matrix to be pseudo-inverted.
+ * @param tol double cutoff for small singular values.
+ * 
+ * @return the pseudo-inverse of a
+*/
+template<typename MatType, typename VecType, 
+    typename DataType = typename MatType::value_type>
+MatType pinv(const MatType& x, double tol = 1.e-6) {
+
+    MatType U;
+    VecType s; 
+    MatType Vt;
+    std::tie(U, s, Vt) = exact_svd<MatType, VecType>(x, true);
+
+    std::size_t num_rows = x.rows(), num_cols = x.cols();
+    MatType s_inv(num_cols, num_rows);
+    s_inv.setZero();
+
+    for(std::size_t i = 0; i < s.size(); i++) {
+        if (s(i) > tol) {
+            s_inv(i, i) = static_cast<DataType>(1) / s(i);
+        }
+        else {
+            s_inv(i, i) = static_cast<DataType>(0);
+        }
+    }
+
+    MatType pinv_mat = Vt * s_inv * U.transpose();
+    return pinv_mat;
+}
+
+
 
 }
 }
