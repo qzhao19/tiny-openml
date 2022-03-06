5da9b6812ce550e681ee3d469305b5e7a00408a2
qzhao19 <qi.zhao@outlook.fr>
2022-03-06 23:41:59 +0800

    Create gaussian_naive_bayes_test.cpp

---
+++ test/gaussian_naive_bayes_test.cpp
@@ -0,0 +1,37 @@
+#include "../src/methods/naive_bayes/gaussian_naive_bayes.hpp"
+using namespace openml;
+
+int main() {
+
+    using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+    MatType X;
+    VecType y;
+
+    data::loadtxt<MatType, VecType>("../dataset/iris.txt", X, y);
+
+    naive_bayes::GaussianNB<double> gaussian_nb;
+
+    gaussian_nb.fit(X, y);
+
+    MatType log_prob = gaussian_nb.predict_log_prob(X);
+    std::cout << "log_prob" << std::endl;
+    std::cout << log_prob << std::endl;
+
+    MatType prob = gaussian_nb.predict_prob(X);
+    std::cout << "prob" << std::endl;
+    std::cout << prob << std::endl;
+
+    VecType pred_y = gaussian_nb.predict(X);
+    std::cout << "pred_y" << std::endl;
+    std::cout << pred_y << std::endl;
+
+    std::cout << "prior prob" << std::endl;
+    std::cout << gaussian_nb.get_prior_prob() << std::endl;
+
+    std::cout << "classes" << std::endl;
+    std::cout << gaussian_nb.get_classes() << std::endl;
+
+
+}
