e404690eb834c8b45a73d588e156ba1a21df361b
qzhao19 <qi.zhao@outlook.fr>
2022-03-06 23:37:07 +0800

    Update core.hpp

--- src/core.hpp
+++ src/core.hpp
@@ -1,14 +1,16 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
-#include "../src/methods/base_estimator.hpp"
+#include "../src/methods/base.hpp"
+
+#include "../src/core/utils.hpp"
+
 
 #include "../src/core/math/shuffle.hpp"
 #include "../src/core/math/linalg.hpp"
 #include "../src/core/math/extmath.hpp"
 #include "../src/core/math/dists.hpp"
 
-#include "../src/core/utils.hpp"
 
 #include "../src/core/data/load.hpp"
 #include "../src/core/data/split.hpp"
@@ -19,8 +21,10 @@
 
 #include "../src/core/optimizer/sgd/sgd.hpp"
 
-
 // #include "../src/core/initializer/zero_initializer.hpp"
 // #include "../src/core/initializer/rand_initializer.hpp"
+
+#include "../src/core/metric.hpp"
+
 
 #endif /*CORE_HPP*/
