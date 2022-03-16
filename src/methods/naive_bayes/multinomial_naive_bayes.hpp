cae574921ee9c607141d724979e87416238ce24f
qzhao19 <qi.zhao@outlook.fr>
2022-03-16 17:44:58 +0800

    Update multinomial_naive_bayes.hpp

--- src/methods/naive_bayes/multinomial_naive_bayes.hpp
+++ src/methods/naive_bayes/multinomial_naive_bayes.hpp
@@ -19,7 +19,6 @@ private:
     double alpha;
 
 protected:
-
     std::size_t num_features;
     MatType feature_count;
     VecType class_count;
@@ -27,7 +26,8 @@ protected:
     void compute_feature_prob(const MatType& X, 
         const VecType& y) {
         
-        std::size_t num_samples = X.rows(), num_features = X.cols();
+        std::size_t num_samples = X.rows();
+        num_features = X.cols();
 
         MatType feature_count_(num_features, this->num_classes);
         VecType class_count_(this->num_classes);
@@ -46,18 +46,14 @@ protected:
                 }
             }
             IdxType keep_cols = IdxType::LinSpaced(X_y.cols(), 0, X_y.cols());
-
             partial_X_y = X_y(keep_rows, keep_cols); 
             feature_count_.col(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features), 0);
             class_count_.row(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features));
-
             new_i++;
         }
-
         feature_count = feature_count_.transpose();
         class_count = class_count_.transpose();
     }
-
 
     /**
      * compute log likelihood function 
@@ -68,38 +64,33 @@ protected:
      * Ny_i  is the count of feature xi in class y
      * Ny  is the total count of all features in class y
     */
-    const double compute_log_likelihood(const VecType& x, const std::size_t c) {
+    const double compute_log_likelihood(const VecType& x, const std::size_t c) const {
 
         double retval = 0.0;
         for (std::size_t i = 0; i < x.rows(); i++) {
             DataType numerator = feature_count(c, i) + alpha;
             DataType denominator = class_count(c, 0) + (alpha * num_features);
-
             retval += x(i) * std::log(numerator / denominator);
         }
+
         return static_cast<double>(retval);
     }
 
-
     const MatType joint_log_likelihood(const MatType& X) const {
         std::size_t num_samples = X.rows();
-        MatType jll(num_samples, this->num_classes);
-        jll.setZero();
+        // define log likelihood matrix
+        MatType ll(num_samples, this->num_classes);
+        ll.setZero();
 
         VecType log_prior = this->prior_prob.array().log();
-        // MatType log_prior_mat = utils::repeat<MatType>(log_prior.transpose(), num_samples, 0);
-        for (std::size_t i = 0; i < num_samples; i++) {
-            
-            VecType tmp(this->num_classes);
-
+        MatType lp = utils::repeat<MatType>(log_prior.transpose(), num_samples, 0);
+        for (std::size_t i = 0; i < num_samples; i++) {        
             for (std::size_t c = 0; c < this->num_classes; c++) {
                 VecType row = X.row(i).transpose();
-                tmp(c) = log_prior(c, 0) + compute_log_likelihood(X.row(i), c);
+                ll(i, c) = compute_log_likelihood(row, c);
             }
-            jll.col(i) = tmp;
         }
-
-        return jll.transpose();
+        return ll + lp;
     };
 
 public:
@@ -108,21 +99,29 @@ public:
      *      Portion of the largest variance of all features.
     */
     MultinomialNB(): BaseNB<DataType>(), 
-        alpha(1.0){};
+        alpha(10.0) {};
 
     MultinomialNB(const double alpha_) : BaseNB<DataType>(),
-            alpha(alpha_){};
+            alpha(alpha_) {};
     
     ~MultinomialNB() {};
 
-
-    void test_func(const MatType& X, 
+    /**
+     * Fit naive bayes classifier according to X, y.
+     *  @param X ndarray of shape (num_samples, num_features)
+     *      input dataset
+    */
+    void fit(const MatType& X, 
         const VecType& y) {
-        
         this->compute_prior_prob(y);
-
         compute_feature_prob(X, y);
+    }
 
+    /**
+     * return uumber of samples encountered for each (class, feature) during fitting.
+    */
+    MatType get_features_count() {
+        return feature_count;
     }
 
 };
