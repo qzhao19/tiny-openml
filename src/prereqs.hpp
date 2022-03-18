10fb7d96395acae9de75e2750386fe9b18c78466
qzhao19 <qi.zhao@outlook.fr>
2022-03-18 22:58:30 +0800

    Update prereqs.hpp

--- src/prereqs.hpp
+++ src/prereqs.hpp
@@ -1,6 +1,8 @@
 #ifndef PREREQS_HPP
 #define PREREQS_HPP
 
+#define _USE_MATH_DEFINES
+
 #include <algorithm>
 #include <cassert>
 #include <cctype>
@@ -25,5 +27,14 @@
 
 #include <eigen3/Eigen/Dense>
 // #include <eigen3/unsupported/Eigen/MatrixFunctions>
+
+#ifndef M_PI
+  #define M_PI 3.141592653589793238462643383279
+#endif
+
+template<typename DataType>
+using ConstType = std::numeric_limits<DataType>;
+
+
 
 #endif /*PREREQS_HPP*/
