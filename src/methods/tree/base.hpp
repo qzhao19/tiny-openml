0c9090505c47ca9e3d397ed598360a0eea2950d3
qzhao19 <qi.zhao@outlook.fr>
2022-03-22 23:52:27 +0800

    Update base.hpp

--- src/methods/tree/base.hpp
+++ src/methods/tree/base.hpp
@@ -8,34 +8,43 @@ namespace openml {
 namespace decision_tree {
 
 template<typename DataType>
-struct Node{
-
+struct Node {
+    bool is_leaf;
     std::size_t num_samples_per_class;
     std::size_t num_samples;
-    std::size_t feature_idx;
+    std::size_t feature_index;
     DataType threshold;
     DataType value;
-    bool is_leaf;
+    
+    std::unique_ptr<Node> left_child;
+    std::unique_ptr<Node> right_child;
 
-    Node* left_child;
-    Node* right_child;
+    Node():is_leaf(false),
+        num_samples_per_class(0),
+        num_samples(0), 
+        feature_index(ConstType<std::size_t>::max()),
+        threshold(ConstType<DataType>::quiet_NaN()),
+        value(ConstType<DataType>::quiet_NaN()),
+        left_child(std::unique_ptr<Node>()), 
+        right_child(std::unique_ptr<Node>()) {};
 
-    Node(std::size_t num_samples_per_class_ = 0, 
+    
+    explicit Node(bool is_leaf_,
+        std::size_t num_samples_per_class_ = 0, 
         std::size_t num_samples_ = 0, 
-        std::size_t feature_idx_ = 0, 
+        std::size_t feature_index_ = ConstType<std::size_t>::max(), 
         DataType threshold_ = ConstType<DataType>::quiet_NaN(), 
-        DataType value_ = ConstType<DataType>::quiet_NaN(), 
-        bool is_leaf_ = false,
-        Node* left_child_ = nullptr, 
-        Node* right_child_ = nullptr): 
+        DataType value_ = ConstType<DataType>::quiet_NaN()): 
+            is_leaf(is_leaf_),
             num_samples_per_class(num_samples_per_class_),
             num_samples(num_samples_), 
-            feature_idx(feature_idx_),
+            feature_index(feature_index_),
             threshold(threshold_),
             value(value_),
-            is_leaf(is_leaf_),
-            left_child(left_child_),
-            right_child(right_child_) {};
+            left_child(std::unique_ptr<Node>()), 
+            right_child(std::unique_ptr<Node>()){};
+
+    ~Node(){};
 };
 
 
@@ -43,11 +52,35 @@ struct Node{
 template<typename DataType>
 class DecisionTree {
 private:
+    // define matrix and vector Eigen type
+    using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
+    using IdxType = Eigen::Vector<Eigen::Index, Eigen::Dynamic>;
+    using NodeType = Node<DataType>;
+    
+
+
     std::size_t min_samples_split;
     std::size_t max_depth;
     double min_impurity;
 
+
+    struct SplitRecords {
+        /* data */
+        double largest_impurity;
+        std::size_t best_feature_index;
+        DataType best_feature_value; 
+        MatType best_left_X;
+        VecType best_left_y;
+        MatType best_right_X;
+        Vectype best_right_y;
+    };
+    
+    std::unique_ptr<NodeType> root;
+
+
 protected:
+    template<typename ImpurityType>
     
 
 
