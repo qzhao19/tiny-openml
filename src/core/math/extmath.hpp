8b6e6bf0b8eebccf143091aaa060f2fb49be5a5e
qzhao19 <qi.zhao@outlook.fr>
2022-04-09 22:39:34 +0800

    Update extmath.hpp

--- src/core/math/extmath.hpp
+++ src/core/math/extmath.hpp
@@ -319,18 +319,18 @@ template<typename VecType,
 double entropy(const VecType& x, 
     const VecType& weight = VecType()) {
 
-    VecType sample_weight = weight;
+    VecType w = weight;
     std::size_t num_rows = x.rows();
-    if (sample_weight.size() == 0) {
-        sample_weight.resize(num_rows);
-        sample_weight.setOnes();
+    if (w.size() == 0) {
+        w.resize(num_rows);
+        w.setOnes();
     }  
 
-    if (sample_weight.size() != 0 && sample_weight.rows() != num_rows) {
+    if (w.size() != 0 && w.rows() != num_rows) {
         char buffer[200];
         std::snprintf(buffer, 200, 
             "Size of sample weights must be equal to x, but got (%ld)",
-            sample_weight.rows());
+            w.rows());
         std::string err_msg = static_cast<std::string>(buffer);
         throw std::invalid_argument(err_msg);
     }
@@ -341,7 +341,7 @@ double entropy(const VecType& x,
 
     for (std::size_t i = 0; i < num_rows; ++i) {
         x_count_map[x(i)]++;
-        w_count_map[x(i)].push_back(sample_weight(i));
+        w_count_map[x(i)].push_back(w(i));
     }
 
     for (auto x_count = x_count_map.begin(), w_count = w_count_map.begin();
@@ -363,67 +363,52 @@ double entropy(const VecType& x,
 };
 
 
-
 template<typename VecType, 
     typename DataType = typename VecType::value_type>
-double cond_entropy(const VecType& x, const VecType& y, VecType& sample_weight) {
+double gini(const VecType& x, 
+    const VecType& weight = VecType()) {
 
+    VecType w = weight;
     std::size_t num_rows = x.rows();
-    if (sample_weight.size() == 0) {
-        sample_weight.resize(num_rows);
-        sample_weight.setOnes();
+    if (w.size() == 0) {
+        w.resize(num_rows);
+        w.setOnes();
     }  
 
-    if (sample_weight.size() != 0 && sample_weight.rows() != num_rows) {
+    if (w.size() != 0 && w.rows() != num_rows) {
         char buffer[200];
         std::snprintf(buffer, 200, 
             "Size of sample weights must be equal to x, but got (%ld)",
-            sample_weight.rows());
+            w.rows());
         std::string err_msg = static_cast<std::string>(buffer);
         throw std::invalid_argument(err_msg);
     }
 
-   
+    double g = 0.0;
+    std::map<DataType, std::size_t> x_count_map;
+    std::map<DataType, std::vector<DataType>> w_count_map;
+
+    for (std::size_t i = 0; i < num_rows; ++i) {
+        x_count_map[x(i)]++;
+        w_count_map[x(i)].push_back(w(i));
+    }
+
+    for (auto x_count = x_count_map.begin(), w_count = w_count_map.begin();
+        x_count != x_count_map.end(), w_count != w_count_map.end(); 
+        ++x_count, ++w_count) {
+        double sum = 0.0;
+        std::size_t num_w_counts = w_count->second.size();
+        for (std::size_t i = 0; i < num_w_counts; ++i) {
+            sum += static_cast<double>(w_count->second[i]);
+        }
+
+        double mean = sum / static_cast<double>(num_w_counts);
+        double p_i = 1.0 * static_cast<double>(x_count->second) * mean / static_cast<double>(num_rows);
+
+        g += std::pow(p_i, 2.0);
+    }
+    return 1.0 - g;
 };
-
-
-template<typename VecType, 
-    typename DataType = typename VecType::value_type>
-double joint_entropy(const VecType& x, 
-    const VecType& y) {
-    
-    VecType sample_weight = weight;
-    std::size_t x_num_rows = x.rows();
-    std::size_t y_num_rows = y.rows();
-
-    if (x_num_rows != y_num_rows) {
-        char buffer[200];
-        std::snprintf(buffer, 200, 
-            "Size of x (%ld) is not equal to y (%ld)",
-            x_num_rows, y_num_rows);
-        std::string err_msg = static_cast<std::string>(buffer);
-        throw std::out_of_range(err_msg);
-    }
-    
-    double joint_ent = 0.0;
-    std::map<std::string, std::size_t> x_y_count_map;
-    std::map<std::string, std::vector<DataType>> w_count_map;
-
-    for (std::size_t i = 0; i < x_num_rows; ++i) {
-        // x_y_count_map[x(i)]++;
-        std::string str_x_y = std::to_string(x(i)) + std::to_string(y(i));
-        x_y_count_map[str_x_y]++;
-    }
-
-    for (auto x_y_count = x_y_count_map.begin(); x_y_count != x_y_count_map.end(); ++x_y_count) {
-        double sum = 0.0;       
-        double p_i = static_cast<double>(x_y_count->second) / static_cast<double>(x_num_rows);
-        joint_ent += (-p_i) * std::log2(p_i);
-    }
-    return joint_ent;
-
-};
-
 
 
 
