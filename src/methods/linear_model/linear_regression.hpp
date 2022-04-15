70a59a3e3b3d15d6259d8aa6f5873734428f7afb
qzhao19 <qi.zhao@outlook.fr>
2022-04-15 22:55:51 +0800

    Update linear_regression.hpp

--- src/methods/linear_model/linear_regression.hpp
+++ src/methods/linear_model/linear_regression.hpp
@@ -2,27 +2,20 @@
 #define METHODS_LINEAR_MODEL_LINEAR_REGRESSION_HPP
 #include "../../prereqs.hpp"
 #include "../../core.hpp"
+#include "base.hpp"
 using namespace openml;
 
 namespace openml{
 namespace linear_model {
 
 template <typename DataType>
-class LinearRegression {
+class LinearRegression: public BaseLinearModel<DataType> {
 private:
     // define matrix and vector Eigen type
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
 protected:
-    /**
-     * @param W: ndarray_like data of shape [num_samples,]. the parameters that we want ot 
-     *           calculate, initialized and filled by constructor for the least square method
-     * @param intercept: bool, default = True. whether to fit the intercept for the model.  
-    */
-    VecType W;
-    bool intercept;
-    
     /**
      * Train the linear regression model, using the sample weights
      * 
@@ -32,89 +25,46 @@ protected:
     void fit_data(const MatType& X, 
         const VecType& y) {
         
-        std::size_t num_samples = X.rows();
-
+        std::size_t num_samples = X.rows(), num_features = X.cols();
         MatType X_new = X;
-        VecType y_new = y;
-
+        
         // if intercept term exists, append a colmun into X
-        if (intercept) {
-            VecType one_mat(num_samples);
-            one_mat.fill(1.0);
-            X_new = utils::hstack<MatType>(X, one_mat);
+        if (this->intercept_) {
+            VecType ones(num_samples);
+            ones.fill(1.0);
+            X_new.conservativeResize(num_samples, num_features + 1);
+            X_new.col(num_features) = ones;
         }
         
-        std::size_t num_features = X_new.cols();
-        // theta = (X.T * X)^(-1) * X.T * y
-        MatType pseudo_inv(num_features, num_features);
-        pseudo_inv = X_new.transpose() * X_new;
-        pseudo_inv = pseudo_inv.inverse();
+        num_features = X_new.cols();
+        // theta = (X.T * X)^(-1) * X.T * y with pseudo_inv
+        MatType pinv(num_features, num_features);
+        pinv = X_new.transpose() * X_new;
+        pinv = pinv.inverse();
 
-        W = pseudo_inv * X_new.transpose() * y_new;
+        this->W_ = pinv * X_new.transpose() * y;
     };
-
-    /**
-     * Calculate the predicted value y_pred for test dataset
-     * 
-     * @param X the test sample
-     * @return Returns predicted values, ndarray of shape (num_samples,)
-    */
-    const VecType predict_label(const MatType& X) const {
-        // y_pred = X * theta
-        std::size_t num_samples = X.rows(), num_features = X.cols();
-        VecType y_pred(num_samples);
-
-        if (intercept) {
-            y_pred = X * W.topRows(num_features);
-            VecType b(num_samples);
-            b = utils::repeat<VecType>(W.bottomRows(1), num_samples, 0);
-            y_pred += b;
-            return y_pred;
-        }
-        else {
-            y_pred = X * W;
-            return y_pred;
-        }
-    }
-
+    
 public:
     /**
      * *empty constructor, we initialize the default value of 
      * the lambda and intercedpt 0.0 and true
     */
-    LinearRegression(): intercept(true){};
+    LinearRegression(): BaseLinearModel<DataType>(true) {};
 
     /**
      * Non-empty constructor, create the model with lamnda and intercept
-     * @param intercept, whether or not to include an intercept term
+     * @param intercept_, whether or not to include an intercept term
     */
-    explicite LinearRegression(const bool intercept_):
-        intercept(intercept_) {};
+    explicit LinearRegression(bool intercept): 
+        BaseLinearModel<DataType>(intercept) {};
 
     /**deconstructor*/
     ~LinearRegression() {};
 
-    /**public fit interface*/
-    void fit(const MatType& X, 
-        const VecType& y) {
-        fit_data(X, y);
-    }
-
-    /**public predict interface*/
-    const VecType predict(const MatType& X) const{
-        VecType y_pred;
-        y_pred = predict_label(X);
-        return y_pred;
-    };
-
-    /**override get_coef interface*/
-    const VecType get_coef() const {
-        return W;
-    };
- 
 };
 
 } // namespace openml
-} // namespace regression
+} // namespace linear_model
 
 #endif /*METHODS_LINEAR_MODEL_LINEAR_REGRESSION_HPP*/
