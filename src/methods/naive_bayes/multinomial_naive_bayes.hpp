34b8d73cbe1672bf84a7abaa0629b63aa79e5683
qzhao19 <qi.zhao@outlook.fr>
2022-04-19 22:47:09 +0800

    Update multinomial_naive_bayes.hpp

--- src/methods/naive_bayes/multinomial_naive_bayes.hpp
+++ src/methods/naive_bayes/multinomial_naive_bayes.hpp
@@ -15,54 +15,53 @@ private:
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
     using IdxType = Eigen::Vector<Eigen::Index, Eigen::Dynamic>;
-
-    double alpha;
+    double alpha_;
 
 protected:
-    std::size_t num_features;
-    MatType feature_count;
-    VecType class_count;
+    std::size_t num_features_;
+    MatType feature_count_;
+    VecType class_count_;
 
     void compute_feature_prob(const MatType& X, 
         const VecType& y) {
         
         std::size_t num_samples = X.rows();
-        num_features = X.cols();
+        num_features_ = X.cols();
 
-        MatType feature_count_(num_features, this->num_classes);
-        VecType class_count_(this->num_classes);
+        MatType feature_count(num_features_, this->num_classes_);
+        VecType class_count(this->num_classes_);
 
-        MatType X_y(num_samples, num_features + 1);
+        MatType X_y(num_samples, num_features_ + 1);
         X_y = utils::hstack<MatType>(X, y);
 
         std::size_t new_i = 0;
-        for (auto& label : this->label_map) {
-            MatType partial_X_y(label.second, num_features + 1);
+        for (auto& label : this->label_map_) {
+            MatType partial_X_y(label.second, num_features_ + 1);
 
             std::vector<std::size_t> keep_rows;
             for (std::size_t i = 0; i < X_y.rows(); ++i) {
-                if (X_y(i, num_features) == label.first) {
+                if (X_y(i, num_features_) == label.first) {
                     keep_rows.push_back(i);
                 }
             }
             IdxType keep_cols = IdxType::LinSpaced(X_y.cols(), 0, X_y.cols());
 
             partial_X_y = X_y(keep_rows, keep_cols); 
-            feature_count_.col(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features), 0);
-            class_count_.row(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features));
+            feature_count.col(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features_), 0);
+            class_count.row(new_i) = math::sum<MatType, VecType>(partial_X_y.leftCols(num_features_));
 
             new_i++;
         }
 
-        feature_count = feature_count_.transpose();
-        class_count = class_count_.transpose();
+        feature_count_ = feature_count.transpose();
+        class_count_ = class_count.transpose();
     }
 
 
     /**
      * compute log likelihood function 
      * 
-     * P(x|c) = (Ny_i + alpha) / (Ny + (alpha * num_features))
+     * P(x|c) = (Ny_i + alpha) / (Ny + (alpha * num_features_))
      * 
      * α  is the smoothing parameter,
      * Ny_i  is the count of feature xi in class y
@@ -72,8 +71,8 @@ protected:
 
         double retval = 0.0;
         for (std::size_t i = 0; i < x.rows(); ++i) {
-            DataType numerator = feature_count(c, i) + alpha;
-            DataType denominator = class_count(c, 0) + (alpha * num_features);
+            DataType numerator = feature_count_(c, i) + alpha_;
+            DataType denominator = class_count_(c, 0) + (alpha_ * num_features_);
             retval += x(i) * std::log(numerator / denominator);
         }
 
@@ -83,13 +82,13 @@ protected:
     const MatType joint_log_likelihood(const MatType& X) const {
         std::size_t num_samples = X.rows();
         // define log likelihood matrix
-        MatType ll(num_samples, this->num_classes);
+        MatType ll(num_samples, this->num_classes_);
         ll.setZero();
 
-        VecType log_prior = this->prior_prob.array().log();
+        VecType log_prior = this->prior_prob_.array().log();
         MatType lp = utils::repeat<MatType>(log_prior.transpose(), num_samples, 0);
         for (std::size_t i = 0; i < num_samples; ++i) {        
-            for (std::size_t c = 0; c < this->num_classes; ++c) {
+            for (std::size_t c = 0; c < this->num_classes_; ++c) {
                 VecType row = X.row(i).transpose();
                 ll(i, c) = compute_log_likelihood(row, c);
             }
@@ -103,16 +102,16 @@ public:
      *      Portion of the largest variance of all features.
     */
     MultinomialNB(): BaseNB<DataType>(), 
-        alpha(10.0) {};
+        alpha_(10.0) {};
 
-    explicit MultinomialNB(const double alpha_) : BaseNB<DataType>(),
-            alpha(alpha_) {};
+    explicit MultinomialNB(double alpha) : BaseNB<DataType>(),
+            alpha_(alpha) {};
     
     ~MultinomialNB() {};
 
     /**
      * Fit naive bayes classifier according to X, y.
-     *  @param X ndarray of shape (num_samples, num_features)
+     *  @param X ndarray of shape (num_samples, num_features_)
      *      input dataset
     */
     void fit(const MatType& X, 
@@ -125,7 +124,7 @@ public:
      * return uumber of samples encountered for each (class, feature) during fitting.
     */
     MatType get_features_count() {
-        return feature_count;
+        return feature_count_;
     }
 
 };
