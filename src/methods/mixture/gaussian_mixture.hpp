c67bf1573fc54868d7dfbfa59fbd2e596cbb4759
qzhao19 <qi.zhao@outlook.fr>
2022-04-24 22:23:33 +0800

    Update gaussian_mixture.hpp

--- src/methods/mixture/gaussian_mixture.hpp
+++ src/methods/mixture/gaussian_mixture.hpp
@@ -1,1 +1,22 @@
+#ifndef METHODS_MIXTURE_GAUSSIAN_MIXTURE_HPP
+#define METHODS_MIXTURE_GAUSSIAN_MIXTURE_HPP
+#include "../../prereqs.hpp"
+#include "../../core.hpp"
+using namespace openml;
 
+namespace openml {
+namespace mixture_model {
+
+class GaussianMixture {
+
+
+
+
+
+
+};
+
+} // mixture_model
+} // openml
+
+#endif /*METHODS_MIXTURE_GAUSSIAN_MIXTURE_HPP*/
