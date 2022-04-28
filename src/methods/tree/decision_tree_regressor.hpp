5285f060ca721e3b96984fd2b79ca85bb6a1c607
qzhao19 <qi.zhao@outlook.fr>
2022-04-28 22:21:53 +0800

    Update decision_tree_regressor.hpp

--- src/methods/tree/decision_tree_regressor.hpp
+++ src/methods/tree/decision_tree_regressor.hpp
@@ -1,5 +1,5 @@
-#ifndef METHODS_TREE_DECISION_TREE_CLASSIFIER_HPP
-#define METHODS_TREE_DECISION_TREE_CLASSIFIER_HPP
+#ifndef METHODS_TREE_DECISION_TREE_REGRESSOR_HPP
+#define METHODS_TREE_DECISION_TREE_REGRESSOR_HPP
 #include "../../prereqs.hpp"
 #include "../../core.hpp"
 #include "base.hpp"
@@ -90,6 +90,112 @@ protected:
             auto right_median = math::median<MatType, VecType>(right_y, -1);
 
         }
+    }
+
+    /**
+     * 
+    */
+    const std::tuple<VecType, DataType> compute_node_value(
+        const VecType& y) const {
+        
+        std::map<DataType, std::size_t> label_count;
+        for (std::size_t i = 0; i < y.rows(); i++) {
+            label_count[y(i)]++;
+        }
+
+        // VecType value;
+        VecType value_eigvec;
+        std::vector<DataType> value_stdvec;
+        for (auto& label : label_count_) {
+            if (label_count.find(label.first) != label_count.end()) {
+                value_stdvec.push_back(label_count.at(label.first));
+            }
+            else {
+                value_stdvec.push_back(0);
+            }
+        }
+
+        value_eigvec = utils::vec2mat<VecType>(value_stdvec);
+        auto value = utils::argmax<MatType, VecType, IdxType>(value_eigvec, -1);
+        return std::make_tuple(value_eigvec, value.value());
+    }
+
+
+    /** Build a decision tree by recursively finding the best split. */
+    void build_tree(const MatType& X, 
+        const VecType& y, 
+        std::shared_ptr<Node>& cur_node, 
+        std::size_t cur_depth) const {
+        
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+        
+        // std::map<DataType, std::size_t> label_count;
+        // for (std::size_t i = 0; i < y.rows(); i++) {
+        //     label_count[y(i)]++;
+        // }
+
+        DataType predict_value;
+        
+        // std::tie(num_samples_per_class, predict_value) = compute_node_value(y);
+        // std::cout << "num_samples_per_class = " << num_samples_per_class.transpose() << std::endl;
+
+        cur_node->is_leaf = true;
+        cur_node->num_samples = num_samples;
+        cur_node->predict_value = predict_value;
+        
+        // stopping split condition
+        if (num_samples < min_samples_split_) {
+            return ;
+        }
+
+        if (cur_depth > max_depth_) {
+            return ;
+        }
+
+        std::size_t best_feature_index = ConstType<std::size_t>::max();
+        DataType best_feature_value = ConstType<DataType>::quiet_NaN();
+        double best_impurity = ConstType<double>::min();
+
+        std::tie(best_impurity, best_feature_index, best_feature_value) = this->best_split(X, y);
+        if (best_feature_index == ConstType<std::size_t>::max()) {
+            return ;
+        }
+
+        if (best_impurity <= min_impurity_decrease_) {
+            return ;
+        }
+
+        cur_node->impurity = best_impurity;
+        cur_node->feature_index = best_feature_index;
+        cur_node->feature_value = best_feature_value;
+        // std::cout << "best_impurity = " << best_impurity << std::endl;
+
+        VecType selected_x = X.col(best_feature_index);
+        IdxType left_selected_index = utils::where<VecType, IdxType>(
+            (selected_x.array() <= best_feature_value)
+        );
+        IdxType right_selected_index = utils::where<VecType, IdxType>(
+            (selected_x.array() > best_feature_value)
+        );
+
+        MatType left_X = X(left_selected_index, Eigen::all);
+        VecType left_y = y(left_selected_index);
+        MatType right_X = X(right_selected_index, Eigen::all);
+        VecType right_y = y(right_selected_index);
+
+        if (left_X.rows() >= min_samples_leaf_) {
+            std::shared_ptr<Node> left_child = std::make_shared<Node>();
+            cur_node->is_leaf = false;
+            cur_node->left_child = left_child;
+            build_tree(left_X, left_y, left_child, cur_depth + 1);
+        }
+
+        if (right_X.rows() >= min_samples_leaf_) {
+            std::shared_ptr<Node> right_child = std::make_shared<Node>();
+            cur_node->is_leaf = false;
+            cur_node->right_child = right_child;
+            build_tree(right_X, right_y, right_child, cur_depth + 1);
+        }
 
     }
 
@@ -122,4 +228,4 @@ public:
 } // decision_tree
 } // openml
 
-#endif /*METHODS_TREE_DECISION_TREE_CLASSIFIER_HPP*/
+#endif /*METHODS_TREE_DECISION_TREE_REGRESSOR_HPP*/
