756a66439dab3c682aaca9da902b1881bba11175
qzhao19 <qi.zhao@outlook.fr>
2022-05-18 23:09:10 +0800

    Update gaussian_mixture.hpp

--- src/methods/mixture/gaussian_mixture.hpp
+++ src/methods/mixture/gaussian_mixture.hpp
@@ -8,6 +8,52 @@ namespace openml {
 namespace mixture_model {
 
 class GaussianMixture {
+private:
+    std::size_t max_iter_;
+    std::size_t n_components_;
+    std::string init_params_;
+    std::string covariance_type_;
+    double tol_;
+    double reg_covar_;
+
+
+    
+
+
+
+
+
+
+
+public:
+    GaussianMixture(): max_iter_(100), 
+        n_components_(1), 
+        init_params_("random"), 
+        covariance_type_("full"), 
+        tol_(1e-3), 
+        reg_covar_(1e-6) {};
+
+    GaussianMixture(std::size_t max_iter,
+        std::size_t n_components,
+        std::string init_params,
+        std::string covariance_type,
+        double tol,
+        double reg_covar): max_iter_(max_iter), 
+            n_components_(n_components), 
+            init_params_(init_params), 
+            covariance_type_(covariance_type), 
+            tol_(tol), 
+            reg_covar_(reg_covar) {};
+
+
+
+
+
+
+
+
+
+
 
 
 
