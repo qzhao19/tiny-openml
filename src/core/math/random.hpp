b7fc2734ee1720e4a2347f0f5fb3716210724fe5
qzhao19 <qi.zhao@outlook.fr>
2022-05-22 22:44:47 +0800

    Create random.hpp

--- src/core/math/random.hpp
+++ src/core/math/random.hpp
@@ -5,29 +5,49 @@
 namespace openml {
 namespace math {
 
-class Random {
+/**
+ * Create an array of the given shape and populate it with random 
+ * samples from a uniform distribution over
+*/
+template<typename MatType, 
+    typename DataType = typename MatType::value_type>
+MatType rand(std::size_t num_rows, std::size_t num_cols) {
+    std::random_device rand;
+    std::mt19937 generator(rand());  //here set the seed
+    std::uniform_real_distribution<DataType> dist{0.0, 1.0};
 
-// private:
-    // std::random_device rand;
-    std::mt19937 generator_;
-
-    std::uniform_real_distribution<double_t> real_dist;
-    std::uniform_int_distribution<std::size_t> int_dist;
-
-
-public:
-    // Random(): Random(std::random_device()) {};
-
-
-
-
-
-
-
-
+    MatType rand_mat = MatType::NullaryExpr(
+        num_rows, num_cols, [&](){
+            return dist(generator);
+        }
+    );
+    return rand_mat;
 };
+
+/**
+ * with random floats sampled from a univariate 
+ * Gaussian distribution of mean 0 and variance 1
+*/
+template<typename MatType, 
+    typename DataType = typename MatType::value_type>
+MatType randn(std::size_t num_rows, std::size_t num_cols) {
+    std::random_device rand;
+    std::mt19937 generator(rand());  //here set the seed
+    std::normal_distribution<DataType> dist{0.0, 1.0};
+
+    MatType rand_mat = MatType::NullaryExpr(
+        num_rows, num_cols, [&](){
+            return dist(generator);
+        }
+    );
+    return rand_mat;
+};
+
+
+
 
 }
 }
 
 #endif /*CORE_MATH_RANDOM_HPP*/
+
