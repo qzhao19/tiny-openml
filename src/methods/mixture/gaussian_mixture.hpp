da5943e3911c5e203ee5a0945f6f1a81a47c35a1
qzhao19 <qi.zhao@outlook.fr>
2022-06-16 21:17:10 +0800

    Update gaussian_mixture.hpp

--- src/methods/mixture/gaussian_mixture.hpp
+++ src/methods/mixture/gaussian_mixture.hpp
@@ -23,9 +23,9 @@ private:
     double tol_;
     double reg_covar_;
     
-
-    std::vector<MatType> precisions_cholesky_;
     std::vector<MatType> covariances_;
+    std::vector<MatType> precisions_;
+    std::vector<MatType> precisions_cholesky_;
     MatType means_;
     VecType weights_;
     double lower_bound_;
@@ -213,7 +213,6 @@ private:
         weighted_log_prob = log_prob + tmp;
 
         return weighted_log_prob;
-
     }
 
 
@@ -305,7 +304,7 @@ private:
             weights) = estimate_gaussian_parameters(X, resp, reg_covar_);
         
         means_ = means;
-        weights = weights * (static_cast<DataType>(1.0) / static_cast<DataType>(num_samples));
+        weights = weights / static_cast<DataType>(num_samples);
         weights_ = weights;
 
         covariances_ = covariances;
@@ -338,6 +337,9 @@ private:
 
     /**
      * M step.
+     * @param X ndarray of shape [num_samples, num_features]
+     * @param log_resp: ndarray of shape [num_samples, num_components]
+     *    log of the post prob of the each samples in X
      * 
     */
     void m_step(const MatType& X, const MatType& log_resp) {
@@ -352,58 +354,54 @@ private:
             weights) = estimate_gaussian_parameters(X, resp, reg_covar_);
 
         means_ = means;
-        weights = weights * (static_cast<DataType>(1.0) / static_cast<DataType>(num_samples));
+        weights = weights / static_cast<DataType>(num_samples);
         weights_ = weights;
 
         covariances_ = covariances;
 
-        precisions_cholesky_ = compute_precision_cholesky(covariances);
+        precisions_cholesky_ = compute_precision_cholesky(covariances_);
     }
-
 
 
     /**
      * Estimate model parameters using X
-     * std::ostringstream err;
-     * err << "Unable to open output file: " << szOutFilePath << std::endl;
-     * throw std::invalid_argument(err.str());
     */
-    void fit_data(const MatType& X){
+    void fit_data(const MatType& X) {
 
         std::size_t num_samples = X.rows(), num_features = X.cols();
         if (num_samples < num_components_) {
-            char buffer[200];
-            std::snprintf(buffer, 200, 
-                "Expected n_samples >= n_components, but got num_components = [%ld], num_samples = [%ld]", 
-                num_components_, num_samples);
-            std::string err_msg = static_cast<std::string>(buffer);
-            throw std::out_of_range(err_msg);
+            std::ostringstream err_msg;
+            err_msg << "Expected n_samples >= n_components, "
+                    << "but got num_components = " << num_components_
+                    << ", num_samples = " << num_samples << std::endl;
+            throw std::out_of_range(err_msg.str());
         }
 
         bool converged = false;
-        double max_lower_bound = ConstType<double>::min();
+        std::size_t best_num_iters;
+        double max_lower_bound = -ConstType<double>::infinity();
         
-
         std::vector<MatType> best_precisions_cholesky;
         std::vector<MatType> best_covariances;
         MatType best_means;
         VecType best_weights;
 
-
         for (std::size_t init = 0; init < num_init_; ++init) {
 
             initialize_parameters(X);
-            double lower_bound = ConstType<double>::min();
-            for (std::size_t iter = 0; iter < max_iter_; ++iter) {
+            double lower_bound = -ConstType<double>::infinity();
+            std::size_t iter;
+            for (iter = 0; iter < max_iter_; ++iter) {
                 double prev_lower_bound = lower_bound;
 
-                DataType mean_log_prob;
+                DataType log_prob_norm;
                 MatType log_resp;
-                std::tie(mean_log_prob, log_resp) = e_step(X);
+                std::tie(log_prob_norm, log_resp) = e_step(X);
                 m_step(X, log_resp);
 
-                lower_bound = static_cast<double>(mean_log_prob);
+                lower_bound = static_cast<double>(log_prob_norm);
                 double diff = lower_bound - prev_lower_bound;
+
                 if (std::abs(diff) < tol_) {
                     converged = true;
                     break;
@@ -411,18 +409,19 @@ private:
             }
 
             if ((lower_bound > max_lower_bound) || 
-                (max_lower_bound == ConstType<double>::min())) {
+                (max_lower_bound == -ConstType<double>::infinity())) {
                 
                 max_lower_bound = lower_bound;
                 best_precisions_cholesky = precisions_cholesky_;
                 best_covariances = covariances_;
                 best_means = means_;
                 best_weights = weights_;
+                best_num_iters = iter;
             }
         }
 
         if (!converged) {
-            std::cout << "Not converge, try different init parameters." << std::endl;
+            std::runtime_error("Not converge, try different init parameters.");
         }
 
         lower_bound_ = max_lower_bound;
@@ -431,18 +430,23 @@ private:
         means_ = best_means;
         weights_ = best_weights;
 
+        for (int i = 0; i < num_components_; ++i) {
+            MatType prec_chol = precisions_cholesky_[i];
+            MatType prec(num_features, num_features);
+            prec = prec_chol * prec_chol.transpose();
+            precisions_.push_back(prec);
+        }
+
         DataType mean_log_prob;
         MatType log_resp;
         std::tie(mean_log_prob, log_resp) = e_step(X);
     }
 
 
-
-
 public:
     GaussianMixture(): max_iter_(100), 
         num_init_(1),
-        num_components_(2), 
+        num_components_(3), 
         init_params_("random"), 
         covariance_type_("full"), 
         tol_(1e-3), 
@@ -463,51 +467,42 @@ public:
             reg_covar_(reg_covar) {};
 
 
+    /**
+     * fit dataset with EM algorithm
+    */
+    void fit(const MatType& X) {
+        fit_data(X);
+    }
 
-    void test_func(const MatType& X) {
+    /**
+     * Predict the labels for the input dataset
+     * @param X  ndarray of shape [num_samples, num_features]
+     *    the input dataset
+    */
+    VecType predict(const MatType& X) {
+        std::size_t num_samples = X.rows();
 
-        initialize_parameters(X);
+        MatType weighted_log_prob(num_samples, num_components_);
+        weighted_log_prob = estimate_weighted_log_prob(X);
 
-        std::size_t num_features = X.cols();
+        auto argmax_index = utils::argmax<MatType, VecType, IdxType>(weighted_log_prob, 1);
 
-        VecType log_det_chol = compute_log_det_cholesky(precisions_cholesky_);
+        VecType pred_y = argmax_index.template cast<DataType>();
+        return pred_y;
+    }
 
-        // std::cout << "log_det_chol" << std::endl;
-        // std::cout << log_det_chol << std::endl;
 
-        MatType log_prob = estimate_log_prob(precisions_cholesky_, X, means_);
+    /**
+     * Evaluate the components' proba for each sample.
+    */
+    MatType predict_prob(const MatType& X) {
 
         MatType log_resp;
         VecType log_prob_norm;
         std::tie(log_resp, log_prob_norm) = estimate_log_prob_resp(X);
 
-        // std::cout << math::mean<MatType, VecType>(log_prob_norm, 0) << std::endl;
-
-        // std::cout << "log_prob" << std::endl;
-        // std::cout << log_prob << std::endl;
-
-        // std::cout << "log_resp" << std::endl;
-        // std::cout << log_resp << std::endl;
-
-        // std::cout << "log_prob_norm" << std::endl;
-        // std::cout << log_prob_norm << std::endl;
-
-        // for(auto& cov : covariances_) {
-        //     std::cout << "cov" << std::endl;
-        //     std::cout << cov << std::endl;
-        // }
-        // std::cout << "means" << std::endl;
-        // std::cout << means_ << std::endl;
-        // std::cout << "weights" << std::endl;
-        // std::cout << weights_ << std::endl;
-        // std::cout << "--------------------------------" << std::endl;
-        // for(auto& precision : precisions_cholesky_) {
-        //     std::cout << "precision" << std::endl;
-        //     std::cout << precision << std::endl;
-        // }
+        return log_resp.array().exp();
     }
-
-
 
 };
 
