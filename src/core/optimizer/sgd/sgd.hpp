07b8a3d276c0dafb323fa260139a50194aa1ceda
qzhao19 <qi.zhao@outlook.fr>
2022-08-11 22:29:21 +0800

    Update sgd.hpp

--- src/core/optimizer/sgd/sgd.hpp
+++ src/core/optimizer/sgd/sgd.hpp
@@ -1,5 +1,7 @@
 #ifndef CORE_OPTIMIZER_SGD_SGD_HPP
 #define CORE_OPTIMIZER_SGD_SGD_HPP
+#include "./decay_policies/step_decay.hpp"
+#include "./decay_policies/exponential_decay.hpp"
 #include "./update_policies/vanilla_update.hpp"
 #include "./update_policies/momentum_update.hpp"
 #include "../../../prereqs.hpp"
@@ -19,11 +21,11 @@ private:
     VecType y_;
     std::size_t max_iter_;
     std::size_t batch_size_;
-    double alpha_;
+    
     double tol_;
     bool shuffle_;
     bool verbose_;
-
+    
     // internal callable parameters
     std::size_t num_samples_;
     std::size_t num_features_;
@@ -32,70 +34,89 @@ private:
 public:
     SGD(const MatType& X, 
         const VecType& y,
-        const std::size_t max_iter = 20000, 
+        const std::size_t max_iter = 2000, 
         const std::size_t batch_size = 16,
-        const double alpha = 0.1,  
         const double tol = 0.0001, 
-        const bool shuffle = true,
+        const bool shuffle = true, 
         const bool verbose = true): X_(X), y_(y), 
             max_iter_(max_iter), 
             batch_size_(batch_size), 
             tol_(tol), 
-            alpha_(alpha), 
-            shuffle_(shuffle), 
+            shuffle_(shuffle),
             verbose_(verbose) {
-                num_samples_ = X.rows();
-                num_features_ = X.cols();
+                num_samples_ = X_.rows();
+                num_features_ = X_.cols();
                 num_batch_ = num_samples_ / batch_size_;
             };
-
     ~SGD() {};
 
     template<typename LossFuncionType, 
-        typename UpdatePolicy>
-    void optimize(LossFuncionType& loss_fn, 
-        UpdatePolicy& update_policy,
-        VecType& W) {
+        typename UpdatePolicyType,
+        typename DecayPolicyType>
+    VecType optimize(const VecType& weights, 
+        LossFuncionType& loss_fn, 
+        UpdatePolicyType& w_update,
+        DecayPolicyType& lr_decay) {
         
-        double total_error = 0.0;
+        double best_loss = 0.0;
         VecType grad(num_features_);
         
-        for (std::size_t i = 0; i < max_iter_; i++) {
-            if (shuffle_) {
-                math::shuffle_data(X_, y_, X_, y_);
-            }
+        bool is_converged = false;
 
+        VecType W = weights;
+        VecType opt_W;
+        for (std::size_t iter = 0; iter < max_iter_; iter++) {
+            if (shuffle_) {
+                random::shuffle_data<MatType, VecType>(X_, y_, X_, y_);
+            }
             MatType X_batch(batch_size_, num_features_);
             VecType y_batch(batch_size_);
-            double error = 0.0;
+            VecType loss_history(batch_size_);
+
+            double lr = lr_decay.compute(iter);
 
             for (std::size_t j = 0; j < num_batch_; j++) {
                 std::size_t begin = j * batch_size_;
-                X_batch = X.middleRows(begin, batch_size_);
-                y_batch = y.middleRows(begin, batch_size_);
+                X_batch = X_.middleRows(begin, batch_size_);
+                y_batch = y_.middleRows(begin, batch_size_);
 
                 grad = loss_fn.gradient(X_batch, y_batch, W);
-                
-                // W = W - alpha * grad;
-                update_policy.update(W, grad, W);
+                // W = W - lr * grad; 
+                W = w_update.update(W, grad, lr);
+                double loss = loss_fn.evaluate(X_batch, y_batch, W);
 
-                error += loss_fn.evaluate(X_batch, y_batch, W);
+                loss_history(j, 0) = loss;
             }
 
-            double average_error = error / static_cast<double>(num_batch_);
-            if (std::abs(total_error - average_error) < tol_) {
+            double avg_loss = static_cast<double>(loss_history.array().mean());
+            
+            if (std::abs(best_loss - avg_loss) < tol_) {
+                is_converged = true;
+                opt_W = W;
                 break;
             } 
-            total_error = average_error;
+
+            best_loss = avg_loss;
             if (verbose_) {
-                if ((i % 10) == 0) {
-                    std::cout << "iter = " << i << ", loss value = " << average_error << std::endl;
+                if ((iter % 10) == 0) {
+                    std::cout << "-- Epoch = " << iter << ", loss value = " << best_loss << std::endl;
                 }
             }
         }
+
+        if (!is_converged) {
+            std::ostringstream err_msg;
+            err_msg << "Not converge, current number of epoch = " << max_iter_
+                    << ", the batch size = " << batch_size_ 
+                    << ", try apply different parameters." << std::endl;
+            throw std::runtime_error(err_msg.str());
+        }
+
+        return opt_W;
     }
+
 };
 
 }
 }
-#endif /*CORE_OPTIMIZER_SGD_SGD_HPP*/
+#endif
