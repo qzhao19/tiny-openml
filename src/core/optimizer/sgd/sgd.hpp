6bd7a242de83aac915cca0c5f13a0c3766b8893d
qzhao19 <qi.zhao@outlook.fr>
2022-08-17 22:16:35 +0800

    update sgd method

--- src/core/optimizer/sgd/sgd.hpp
+++ src/core/optimizer/sgd/sgd.hpp
@@ -21,6 +21,7 @@ private:
     VecType y_;
     std::size_t max_iter_;
     std::size_t batch_size_;
+    std::size_t num_iters_no_change_;
     
     double tol_;
     bool shuffle_;
@@ -30,17 +31,20 @@ private:
     std::size_t num_samples_;
     std::size_t num_features_;
     std::size_t num_batch_;
+    DataType MAX_DLOSS = static_cast<DataType>(1e+10);
 
 public:
     SGD(const MatType& X, 
         const VecType& y,
         const std::size_t max_iter = 2000, 
         const std::size_t batch_size = 16,
+        const std::size_t num_iters_no_change = 5,
         const double tol = 0.0001, 
         const bool shuffle = true, 
         const bool verbose = true): X_(X), y_(y), 
             max_iter_(max_iter), 
-            batch_size_(batch_size), 
+            batch_size_(batch_size),
+            num_iters_no_change_(num_iters_no_change),
             tol_(tol), 
             shuffle_(shuffle),
             verbose_(verbose) {
@@ -58,11 +62,12 @@ public:
         UpdatePolicyType& w_update,
         DecayPolicyType& lr_decay) {
         
-        double best_loss = 0.0;
-        VecType grad(num_features_);
-        
         bool is_converged = false;
-
+        std::size_t no_improvement_count = 0;
+        
+        double best_loss = ConstType<double>::infinity();
+        
+        
         VecType W = weights;
         VecType opt_W;
         for (std::size_t iter = 0; iter < max_iter_; iter++) {
@@ -80,7 +85,13 @@ public:
                 X_batch = X_.middleRows(begin, batch_size_);
                 y_batch = y_.middleRows(begin, batch_size_);
 
+
+                VecType grad(num_features_);
                 grad = loss_fn.gradient(X_batch, y_batch, W);
+
+                // clip gradient with large value 
+                grad = utils::clip<MatType>(grad, MAX_DLOSS, -MAX_DLOSS);
+
                 // W = W - lr * grad; 
                 W = w_update.update(W, grad, lr);
                 double loss = loss_fn.evaluate(X_batch, y_batch, W);
@@ -88,18 +99,29 @@ public:
                 loss_history(j, 0) = loss;
             }
 
-            double avg_loss = static_cast<double>(loss_history.array().mean());
-            
-            if (std::abs(best_loss - avg_loss) < tol_) {
+            double sum_loss = static_cast<double>(loss_history.array().sum());
+
+            if (sum_loss > best_loss - tol_ * batch_size_) {
+                no_improvement_count +=1;
+            }
+            else {
+                no_improvement_count = 0;
+            }
+
+            if (sum_loss < best_loss) {
+                best_loss = sum_loss;
+            }
+
+            if (no_improvement_count >= num_iters_no_change_) {
                 is_converged = true;
                 opt_W = W;
                 break;
-            } 
+            }
 
-            best_loss = avg_loss;
             if (verbose_) {
-                if ((iter % 10) == 0) {
-                    std::cout << "-- Epoch = " << iter << ", loss value = " << best_loss << std::endl;
+                if ((iter % 2) == 0) {
+                    std::cout << "-- Epoch = " << iter << ", average loss value = " 
+                            << sum_loss / static_cast<double>(batch_size_) << std::endl;
                 }
             }
         }
