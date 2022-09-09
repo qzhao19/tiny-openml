5e172f67ec5ba9045414a7f484a94d1094a0763d
qzhao19 <qi.zhao@outlook.fr>
2022-09-09 23:51:12 +0800

    update lasso regression

---
+++ src/methods/linear_model/lasso_regression.hpp
@@ -0,0 +1,73 @@
+#ifndef METHODS_LINEAR_MODEL_RIDGE_REGRESSION_HPP
+#define METHODS_LINEAR_MODEL_RIDGE_REGRESSION_HPP
+#include "../../prereqs.hpp"
+#include "../../core.hpp"
+#include "base.hpp"
+using namespace openml;
+
+namespace openml{
+namespace linear_model {
+
+template <typename DataType>
+class LassoRegression: public BaseLinearModel<DataType> {
+private:
+    // define matrix and vector Eigen type
+    using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
+    
+protected:
+    double lambda_;
+
+    /**fit data implementation*/
+    void fit_data(const MatType& X, 
+        const VecType& y) {
+        
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+        MatType X_new = X;
+        
+        // if intercept term exists, append a colmun into X
+        if (this->intercept_) {
+            VecType ones(num_samples);
+            ones.fill(1.0);
+            X_new.conservativeResize(num_samples, num_features + 1);
+            X_new.col(num_features) = ones;
+        }
+        
+        num_features = X_new.cols();
+        // theta = (X.T * X + lambda * eye)^(-1) * X.T * y
+        MatType eyes(num_features, num_features);
+        eyes.setIdentity();
+        
+        
+
+        this->W_ = pinv * X_new.transpose() * y;
+    };
+
+public:
+    /**
+     * *empty constructor, we initialize the default value of 
+     * the lambda and intercedpt 0.0 and true
+    */
+    LassoRegression(): BaseLinearModel<DataType>(true), 
+        lambda_(0.5) {};
+
+    /**
+     * Non-empty constructor, create the model with lamnda and intercept
+     * @param lambda: The penalty (aka regularization term) to be used. 
+     *      Constant that multiplies the regularization term
+     * @param intercept: bool, default = True. whether to fit the intercept for the model. 
+    */
+    LassoRegression(const double lambda, 
+        const bool intercept): 
+            BaseLinearModel<DataType>(intercept), 
+            lambda_(lambda) {};
+
+    /**deconstructor*/
+    ~LassoRegression() {};
+
+};
+
+} // namespace openml
+} // namespace regression
+
+#endif /*METHODS_LINEAR_MODEL_RIDGE_REGRESSION_HPP*/
