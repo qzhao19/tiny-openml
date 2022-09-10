b564b97fa9d8631ebc0b1f7ec589b25a5c29cef5
qzhao19 <qi.zhao@outlook.fr>
2022-09-10 23:41:56 +0800

    change mse test

--- test/mse_test.cpp
+++ test/mse_test.cpp
@@ -5,23 +5,17 @@ int main() {
 
     using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
-
-    MatType X;
-    VecType y;
-
-    data::loadtxt<MatType, VecType>("../dataset/boston_house_price.txt", X, y);
-
-    MatType X_train;
-    MatType X_test;
-    VecType y_train;
-    VecType y_test;
-    std::tie(X_train, X_test, y_train, y_test) = data::train_test_split<MatType, VecType>(X, y, 0.9);
+    
+    MatType X{{0, 0, 0, 0.2, 0.2}, 
+              {0, 0, 0.3, 0, -0.5},
+              {0.4, 0.6, 0, 0, 0}};
+    VecType y{{1, 1, -1}};
 
     std::size_t num_samples = X.rows(), num_features = X.cols();
 
     loss::MSE<double> mse;
     VecType W(num_features);
-    W = Eigen::MatrixXd::Ones(num_features, 1);
+    W.setZero();
     VecType grad(num_features);
     
     double loss_val = 0.0;
