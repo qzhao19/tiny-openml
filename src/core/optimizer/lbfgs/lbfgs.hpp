0707657a5516ad148755d4cec9e89040bb43b3bc
qzhao19 <qi.zhao@outlook.fr>
2022-09-18 23:50:06 +0800

    create lbfgs optimization method

---
+++ src/core/optimizer/lbfgs/lbfgs.hpp
@@ -0,0 +1,20 @@
+#ifndef CORE_OPTIMIZER_LBFGS_LBFGS_HPP
+#define CORE_OPTIMIZER_LBFGS_LBFGS_HPP
+#include "../../../prereqs.hpp"
+#include "../../../core.hpp"
+using namespace openml;
+
+namespace openml {
+namespace optimizer {
+
+/**
+ * Stochastic coordinate descent algorithm
+*/
+template<typename DataType>
+class LBFGS {
+    
+};
+
+}
+}
+#endif /*CORE_OPTIMIZER_LBFGS_LBFGS_HPP*/
