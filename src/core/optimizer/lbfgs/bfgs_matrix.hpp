d7e610c2347563ec45d382fd6e767374591c753f
qzhao19 <qi.zhao@outlook.fr>
2022-09-28 22:50:43 +0800

    rename hessian_matrix to bfgs_matrix

---
+++ src/core/optimizer/lbfgs/bfgs_matrix.hpp
@@ -0,0 +1,17 @@
+#ifndef CORE_OPTIMIZER_LBFGS_BFGS_MATRIX_HPP
+#define CORE_OPTIMIZER_LBFGS_BFGS_MATRIX_HPP
+#include "../../../prereqs.hpp"
+#include "../../../core.hpp"
+
+namespace openml {
+namespace optimizer {
+
+template<typename DataType>
+class BFGSMatrix {
+
+};
+
+}
+}
+
+#endif  // CORE_OPTIMIZER_LBFGS_BFGS_MATRIX_HPP
