21b87daf2cb3d3e12735ef8ded63f7bae2e64df0
qzhao19 <qi.zhao@outlook.fr>
2022-09-28 22:50:05 +0800

    update perceptron method

--- src/methods/linear_model/perceptron.hpp
+++ src/methods/linear_model/perceptron.hpp
@@ -16,7 +16,15 @@ private:
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
     
 protected:
+
+    bool shuffle_;
+    bool verbose_;
+    double alpha_;
+    double tol_;
     double lambda_;
+    std::size_t batch_size_;
+    std::size_t max_iter_;
+    std::string penalty_;
 
     /**fit data implementation*/
     void fit_data(const MatType& X, 
@@ -29,7 +37,14 @@ public:
      * the lambda and intercedpt 0.0 and true
     */
     Perceptron(): BaseLinearModel<DataType>(true), 
-        lambda_(0.5) {};
+        shuffle_(true), 
+        verbose_(false), 
+        alpha_(0.001), 
+        lambda_(0.5), 
+        tol_(0.0001), 
+        batch_size_(32), 
+        max_iter_(1000), 
+        penalty_("l2") {};
 
     /**
      * Non-empty constructor, create the model with lamnda and intercept
@@ -37,10 +52,24 @@ public:
      *      Constant that multiplies the regularization term
      * @param intercept: bool, default = True. whether to fit the intercept for the model. 
     */
-    Perceptron(const double lambda, 
-        const bool intercept): 
+    Perceptron(bool intercept,
+        bool shuffle, 
+        bool verbose, 
+        const double alpha, 
+        const double lambda,
+        const double tol, 
+        const std::size_t batch_size, 
+        const std::size_t max_iter, 
+        const std::string penalty): 
             BaseLinearModel<DataType>(intercept), 
-            lambda_(lambda) {};
+            shuffle_(shuffle), 
+            verbose_(verbose), 
+            alpha_(alpha), 
+            lambda_(lambda), 
+            tol_(tol), 
+            batch_size_(batch_size), 
+            max_iter_(max_iter), 
+            penalty_(penalty_) {};
 
     /**deconstructor*/
     ~Perceptron() {};
