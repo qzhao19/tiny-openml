95dd0da57f018daad9d0b2d090f5112ecb65905b
qzhao19 <qi.zhao@outlook.fr>
2022-10-11 21:18:04 +0800

    finish test file

--- test/perceptron_test.cpp
+++ test/perceptron_test.cpp
@@ -16,6 +16,19 @@ int main() {
     std::size_t num_features = X.cols();
     
     std::cout << "perceptron test" <<std::endl;
+    MatType X_train;
+    MatType X_test;
+    VecType y_train;
+    VecType y_test;
+    std::tie(X_train, X_test, y_train, y_test) = data::train_test_split<MatType, VecType>(X, y, 0.95);
+
+    linear_model::Perceptron<double> perceptron;   
+    perceptron.fit(X_train, y_train);
+
+    VecType y_pred;
+    y_pred = perceptron.predict(X_test);
+    std::cout << "y_pred" << std::endl;
+    std::cout << y_pred << std::endl;
     
 }
 
