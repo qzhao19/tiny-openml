7e54fe2ca8959cbda86124b0d8471131ca8c050e
qzhao19 <qi.zhao@outlook.fr>
2022-10-17 22:08:22 +0800

    add line search bracketing policy

---
+++ src/core/optimizer/lbfgs/search_policies/line_search_bracketing.hpp
@@ -0,0 +1,18 @@
+#ifndef CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_LINE_SEARCH_BRACKETING_HPP
+#define CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_LINE_SEARCH_BRACKETING_HPP
+
+namespace openml {
+namespace optimizer {
+
+template <typename DataType>
+class LineSearchBracketing {
+
+
+
+
+};
+
+}
+}
+
+#endif /* CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_LINE_SEARCH_BRACKETING_HPP */
