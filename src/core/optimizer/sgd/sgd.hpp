ecce9e5b55dac7f7f99306384aa2e630cb9b235e
qzhao19 <qi.zhao@outlook.fr>
2022-11-28 23:06:43 +0800

    refactor sgd class

--- src/core/optimizer/sgd/sgd.hpp
+++ src/core/optimizer/sgd/sgd.hpp
@@ -11,86 +11,84 @@ using namespace openml;
 namespace openml {
 namespace optimizer {
 
-template<typename DataType>
+template<typename DataType, 
+    typename LossFuncionType, 
+    typename UpdatePolicyType,
+    typename DecayPolicyType>
 class SGD {
 private:
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
-    MatType X_;
-    VecType y_;
+    VecType x0_;
     std::size_t max_iter_;
     std::size_t batch_size_;
     std::size_t num_iters_no_change_;
-    
     double tol_;
     bool shuffle_;
     bool verbose_;
     
     // internal callable parameters
-    std::size_t num_samples_;
-    std::size_t num_features_;
-    std::size_t num_batch_;
     DataType MAX_DLOSS = static_cast<DataType>(1e+10);
 
 public:
-    SGD(const MatType& X, 
-        const VecType& y,
-        const std::size_t max_iter = 2000, 
-        const std::size_t batch_size = 128,
+    SGD(
+        const std::size_t max_iter = 2000,
+        const std::size_t batch_size = 16,
         const std::size_t num_iters_no_change = 5,
         const double tol = 0.0001, 
         const bool shuffle = true, 
-        const bool verbose = true): X_(X), y_(y), 
+        const bool verbose = true):
             max_iter_(max_iter), 
             batch_size_(batch_size),
             num_iters_no_change_(num_iters_no_change),
             tol_(tol), 
             shuffle_(shuffle),
-            verbose_(verbose) {
-                num_samples_ = X_.rows();
-                num_features_ = X_.cols();
-                num_batch_ = num_samples_ / batch_size_;
-            };
+            verbose_(verbose) {};
     ~SGD() {};
 
-    template<typename LossFuncionType, 
-        typename UpdatePolicyType,
-        typename DecayPolicyType>
-    VecType optimize(const VecType& weights, 
-        LossFuncionType& loss_fn, 
-        UpdatePolicyType& w_update,
-        DecayPolicyType& lr_decay) {
+    VecType optimize(const MatType& X, 
+        const VecType& y, 
+        const VecType& weight) {
         
-        bool is_converged = false;
+        LossFuncionType loss_fn;
+        UpdatePolicyType w_update;
+        DecayPolicyType lr_decay;
+
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+        std::size_t num_batch = num_samples / batch_size_;
         std::size_t no_improvement_count = 0;
+
+        bool is_converged = false;
         double best_loss = ConstType<double>::infinity();
-        
-        VecType grad(num_features_);
-        VecType W = weights;
-        VecType opt_W;
+
+        MatType X_new = X;
+        VecType y_new = y;
+        VecType grad(num_features);
+        VecType w = weight;
+        VecType opt_w;
         for (std::size_t iter = 0; iter < max_iter_; iter++) {
             if (shuffle_) {
-                random::shuffle_data<MatType, VecType>(X_, y_, X_, y_);
+                random::shuffle_data<MatType, VecType>(X_new, y_new, X_new, y_new);
             }
-            MatType X_batch(batch_size_, num_features_);
+            MatType X_batch(batch_size_, num_features);
             VecType y_batch(batch_size_);
             VecType loss_history(batch_size_);
 
             double lr = lr_decay.compute(iter);
 
-            for (std::size_t j = 0; j < num_batch_; j++) {
+            for (std::size_t j = 0; j < num_batch; j++) {
                 std::size_t begin = j * batch_size_;
-                X_batch = X_.middleRows(begin, batch_size_);
-                y_batch = y_.middleRows(begin, batch_size_);
+                X_batch = X_new.middleRows(begin, batch_size_);
+                y_batch = y_new.middleRows(begin, batch_size_);
 
-                grad = loss_fn.gradient(X_batch, y_batch, W);
+                grad = loss_fn.gradient(X_batch, y_batch, w);
                 // clip gradient with large value 
                 grad = utils::clip<MatType>(grad, MAX_DLOSS, -MAX_DLOSS);
 
                 // W = W - lr * grad; 
-                W = w_update.update(W, grad, lr);
-                double loss = loss_fn.evaluate(X_batch, y_batch, W);
+                w = w_update.update(w, grad, lr);
+                double loss = loss_fn.evaluate(X_batch, y_batch, w);
                 loss_history(j, 0) = loss;
             }
 
@@ -109,7 +107,7 @@ public:
 
             if (no_improvement_count >= num_iters_no_change_) {
                 is_converged = true;
-                opt_W = W;
+                opt_w = w;
                 break;
             }
 
@@ -129,7 +127,7 @@ public:
             throw std::runtime_error(err_msg.str());
         }
 
-        return opt_W;
+        return opt_w;
     }
 
 };
