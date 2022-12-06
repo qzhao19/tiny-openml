8ada92c9134b7bc42e828acde108640dc74e8652
qzhao19 <qi.zhao@outlook.fr>
2022-12-06 23:50:10 +0800

    update scd

--- src/core/optimizer/sgd/scd.hpp
+++ src/core/optimizer/sgd/scd.hpp
@@ -14,102 +14,105 @@ namespace optimizer {
 /**
  * Stochastic coordinate descent algorithm
 */
-template<typename DataType>
-class SCD {
+template<typename DataType, 
+    typename LossFuncionType, 
+    typename UpdatePolicyType,
+    typename DecayPolicyType>
+class SCD: public BaseOptimizer<DataType, 
+    LossFuncionType, 
+    UpdatePolicyType, 
+    DecayPolicyType> {
 private:
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
-    MatType X_;
-    VecType y_;
-    std::size_t max_iter_;
+    // LossFuncionType loss_func_;
+    // std::size_t max_iter_;
     
-    double rho_;
     double lambda_;
+    double l1_ratio_;
     
-    bool shuffle_;
-    bool verbose_;
-
-    std::size_t num_samples_;
-    std::size_t num_features_;
+    // bool shuffle_;
+    // bool verbose_;
 
 public:
-    SCD(const MatType& X, 
-        const VecType& y,
+    SCD(const VecType& x0,
+        const LossFuncionType& loss_func,
         const std::size_t max_iter = 5000, 
-        const double rho = 1.0, 
-        const double lambda = 0.001, 
+        const double l1_ratio = 1.0, 
+        const double lambda = 0.0001,
         const bool shuffle = true,
-        const bool verbose = true): X_(X), y_(y), 
-            max_iter_(max_iter), 
-            rho_(rho), 
+        const bool verbose = true): BaseOptimizer<DataType, 
+            LossFuncionType, 
+            UpdatePolicyType, 
+            DecayPolicyType>(x0, 
+                loss_func, 
+                max_iter, 
+                shuffle, 
+                verbose),
             lambda_(lambda),
-            shuffle_(shuffle), 
-            verbose_(verbose) {
-                num_samples_ = X_.rows();
-                num_features_ = X_.cols();
-            };
+            l1_ratio_(l1_ratio) {};
     ~SCD() {};
 
-    template<typename LossFuncionType>
-    VecType optimize(const VecType& weights, 
-        LossFuncionType& loss_fn) {
-        
-        double eta = 0;
-        std::size_t feature_index = 0;
-        
-        VecType grad(num_features_);
-        VecType W = weights;
 
-        for (std::size_t iter = 0; iter < max_iter_; iter++) {
-            if (shuffle_) {
-                random::shuffle_data<MatType, VecType>(X_, y_, X_, y_);
+    void optimize(const MatType& X, 
+        const VecType& y) {
+        
+        double eta = 0.0;
+        std::size_t feat_index = 0;
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+
+        MatType X_new = X;
+        VecType y_new = y;
+        VecType grad(num_features);
+
+        for (std::size_t iter = 0; iter < this->max_iter_; iter++) {
+            if (this->shuffle_) {
+                random::shuffle_data<MatType, VecType>(X_new, y_new, X_new, y_new);
             }
 
-            grad = loss_fn.gradient(X_, y_, W);
+            grad = loss_func_.gradient(X_new, y_new, w);
 
             double pred_descent = 0.0;
             double best_descent = -1.0;
             double best_eta = 0.0;
             std::size_t best_index = 0.0;
             
-            for (feature_index = 0; feature_index < num_features_; ++feature_index) {
-                if ((W(feature_index, 0) - grad(feature_index, 0) / rho_) > (lambda_ / rho_)) {
-                    eta = (-grad(feature_index, 0) / rho_) - (lambda_ / rho_);
+            for (feat_index = 0; feat_index < num_features; ++feat_index) {
+                if ((this->x0_(feat_index, 0) - grad(feat_index, 0) / l1_ratio_) > (lambda_ / l1_ratio_)) {
+                    eta = (-grad(feat_index, 0) / l1_ratio_) - (lambda_ / l1_ratio_);
                 }
-                else if ((W(feature_index, 0) - grad(feature_index, 0) / rho_) < (-lambda_ / rho_)) {
-                    eta = (-grad(feature_index, 0) / rho_) + (lambda_ / rho_);
+                else if ((this->x0_(feat_index, 0) - grad(feat_index, 0) / l1_ratio_) < (-lambda_ / l1_ratio_)) {
+                    eta = (-grad(feat_index, 0) / l1_ratio_) + (lambda_ / l1_ratio_);
                 }
                 else {
-                    eta = -W(feature_index, 0);
+                    eta = -this->x0_(feat_index, 0);
                 }
 
-                pred_descent = -eta * grad(feature_index, 0) - 
-                    rho_ / 2 * eta * eta - 
-                        lambda_ * std::abs(W(feature_index, 0) + eta) + 
-                            lambda_ * std::abs(W(feature_index, 0));
+                pred_descent = -eta * grad(feat_index, 0) - 
+                    l1_ratio_ / 2 * eta * eta - 
+                        lambda_ * std::abs(this->x0_(feat_index, 0) + eta) + 
+                            lambda_ * std::abs(this->x0_(feat_index, 0));
 
                 if (pred_descent > best_descent) {
                     best_descent = pred_descent;
-                    best_index = feature_index;
+                    best_index = feat_index;
                     best_eta = eta;
                 }
             }
-            feature_index = best_index;
+            feat_index = best_index;
             eta = best_eta;
-            W(feature_index, 0) += eta;
+            this->x0_(feat_index, 0) += eta;
 
-            if (verbose_) {
+            if (this->verbose_) {
                 if ((iter % 100) == 0) {
-                    double w_norm = W.array().abs().sum();
-                    double loss = loss_fn.evaluate(X_, y_, W);
-
+                    double w_norm = this->x0_.array().abs().sum();
+                    double loss = loss_func_.evaluate(X_new, y_new, this->x0_);
                     std::cout << "-- Epoch = " << iter << ", weight norm = " 
-                        << w_norm <<", loss value = " << loss / num_samples_ << std::endl;
+                        << w_norm <<", loss value = " << loss / num_samples << std::endl;
                 }
             }
         }
-        return W;
     } 
 };
 
