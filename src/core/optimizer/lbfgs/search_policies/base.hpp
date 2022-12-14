bdc7d0ef58c0b18654cac5c6c1da308f202a0aa2
qzhao19 <qi.zhao@outlook.fr>
2022-12-14 23:46:39 +0800

    update base class

--- src/core/optimizer/lbfgs/search_policies/base.hpp
+++ src/core/optimizer/lbfgs/search_policies/base.hpp
@@ -6,18 +6,43 @@
 namespace openml {
 namespace optimizer {
 
-template <typename DataType>
-class BaseLineSearch {
+template <typename DataType,
+    typename LossFuncionType,
+    typename LineSearchParamType>
+class LineSearch {
 private:
     // define matrix and vector Eigen type
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
+protected:
+    MatType X_;
+    VecType y_;
+    LossFuncionType loss_func_;
+    LineSearchParamType linesearch_params_;
 
+public:
+    LineSearch() {};
+    LineSearch(const MatType& X, 
+        const VecType& y,
+        const LossFuncionType& loss_func,
+        const LineSearchParamType& linesearch_params): X_(X), 
+            y_(y), 
+            loss_func_(loss_func), 
+            linesearch_params_(linesearch_params) {};
+
+    ~LineSearch() {};
+
+    virtual int search(VecType& x, 
+        double& fx, 
+        VecType& g, 
+        VecType& d, 
+        double& step, 
+        const VecType& xp, 
+        const VecType& gp) = 0;
 
 };
 
 }
 }
-
 #endif /* CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_BASE_HPP */
