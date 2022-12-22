cfe9684fd346badd983f306f148187a20348f1af
qzhao19 <qi.zhao@outlook.fr>
2022-12-22 22:59:16 +0800

    update

--- src/core/optimizer/lbfgs/search_policies/backtracking.hpp
+++ src/core/optimizer/lbfgs/search_policies/backtracking.hpp
@@ -2,18 +2,46 @@
 #define CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_BACKTRACKING_HPP
 #include "../../../../prereqs.hpp"
 #include "../../../../core.hpp"
+#include "./base.hpp"
 
 namespace openml {
 namespace optimizer {
 
-template <typename DataType>
-class LineSearchBacktracking {
+template <typename DataType,
+    typename LossFuncionType,
+    typename LineSearchParamType>
+class LineSearchBacktracking: public LineSearch<DataType, 
+    LossFuncionType, 
+    LineSearchParamType> {
 private:
     // define matrix and vector Eigen type
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
+public:
+    LineSearchBacktracking(const MatType& X, 
+        const VecType& y,
+        const LossFuncionType& loss_func,
+        const LineSearchParamType& linesearch_params): LineSearch<DataType, 
+            LossFuncionType, 
+            LineSearchParamType>(
+                X, y, 
+                loss_func, 
+                linesearch_params
+        ) {};
+    
+    ~LineSearchBacktracking() {};
 
+    int search(VecType& x, 
+        double& fx, 
+        VecType& g, 
+        VecType& d, 
+        double& step, 
+        const VecType& xp, 
+        const VecType& gp) {
+        
+
+    }
 
 };
 
--- src/core/optimizer/lbfgs/search_policies/bracketing.hpp
+++ src/core/optimizer/lbfgs/search_policies/bracketing.hpp
@@ -2,20 +2,49 @@
 #define CORE_OPTIMIZER_LBFGS_SEARCH_POLICIES_BRACKETING_HPP
 #include "../../../../prereqs.hpp"
 #include "../../../../core.hpp"
+#include "./base.hpp"
 
 namespace openml {
 namespace optimizer {
 
-template <typename DataType>
-class LineSearchBracketing {
+template <typename DataType,
+    typename LossFuncionType,
+    typename LineSearchParamType>
+class LineSearchBracketing: public LineSearch<DataType, 
+    LossFuncionType, 
+    LineSearchParamType> {
 private:
     // define matrix and vector Eigen type
     using MatType = Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>;
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
 
+public:
+    LineSearchBracketing(const MatType& X, 
+        const VecType& y,
+        const LossFuncionType& loss_func,
+        const LineSearchParamType& linesearch_params): LineSearch<DataType, 
+            LossFuncionType, 
+            LineSearchParamType>(
+                X, y, 
+                loss_func, 
+                linesearch_params
+        ) {};
+    
+    ~LineSearchBracketing() {};
 
+    int search(VecType& x, 
+        double& fx, 
+        VecType& g, 
+        VecType& d, 
+        double& step, 
+        const VecType& xp, 
+        const VecType& gp) {
+        
+        
+    }
 
 };
+
 
 }
 }
