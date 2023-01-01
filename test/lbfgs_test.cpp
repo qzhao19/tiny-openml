9b665fb3a3fc35782a7c5218f11a72da5ebc9917
qzhao19 <qi.zhao@outlook.fr>
2023-01-01 23:27:21 +0800

    add test file for lbfgs

---
+++ test/lbfgs_test.cpp
@@ -0,0 +1,37 @@
+#include "../src/core/loss/log_loss.hpp"
+#include "../src/core/optimizer/lbfgs/lbfgs.hpp"
+#include "../src/core/optimizer/lbfgs/params.hpp"
+
+using namespace openml;
+
+int main() {
+
+    using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+    MatType X;
+    VecType y;
+
+    data::loadtxt<MatType, VecType>("../dataset/ionosphere.txt", X, y);
+
+    std::size_t num_features = X.cols();
+    std::cout << "lbfgs test" <<std::endl;
+    
+    VecType w(num_features);
+    w.setZero();
+    VecType opt_w(num_features);
+
+    loss::LogLoss<double> log_loss;
+    optimizer::LinearSearchParams<double> linear_search_params;
+
+    optimizer::LBFGS<double, 
+        loss::LogLoss<double>, 
+        optimizer::LinearSearchParams<double>> lbfgs(w, log_loss, linear_search_params);
+    
+    lbfgs.optimize(X, y);
+    opt_w = lbfgs.get_coef();
+    
+    std::cout << opt_w <<std::endl;
+
+}
+
