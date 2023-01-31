839601ae97b56a086c7ee48e3cc100f37cba4d27
qzhao19 <qi.zhao@outlook.fr>
2023-01-31 23:30:06 +0800

    add softmax loss test

---
+++ test/softmax_loss_test.cpp
@@ -0,0 +1,17 @@
+#include "../src/core/loss/softmax_loss.hpp"
+using namespace openml;
+
+int main() {
+
+    using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+    MatType X;
+    VecType y;
+
+    data::loadtxt<MatType, VecType>("../dataset/iris.txt", X, y);
+
+    std::size_t num_samples = X.rows(), num_features = X.cols();
+    
+
+}
