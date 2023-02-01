3e7421ff395a2fcfb480b77e1a5d933f94590e42
qzhao19 <qi.zhao@outlook.fr>
2023-02-01 23:50:51 +0800

    update

--- src/core/loss/softmax_loss.hpp
+++ src/core/loss/softmax_loss.hpp
@@ -32,34 +32,74 @@ public:
     */
     const double evaluate(const MatType& X, 
         const VecType& y, 
-        const MatType& w) const {
+        const MatType& W) const {
         
-        std::size_t num_samples = X.rows();
-        std::size_t num_features = X.cols();
-        std::size_t num_classes = w.cols();
+        std::size_t num_classes = W.cols();
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+        
         MatType xw(num_samples, num_classes);
-        xw = X.dot(w);
+        xw = X * W;
 
-        xw.noalias() -= xw.maxCoeff();
-        MatType exp_xw = xw.array().exp();
-
+        auto tmp1 = xw.array() - xw.maxCoeff();
+        MatType exp_xw = tmp1.array().exp();
         VecType sum_exp_xw = exp_xw.rowwise().sum();
 
+        std::size_t index;
+        VecType exp_xw_i(num_samples);
+        for (int i = 0; i < num_samples; ++i) {
+            index = static_cast<std::size_t>(y(i, 0));
+            exp_xw_i(i, 0) = exp_xw(i, index);
+        }
+
+        auto tmp2 = exp_xw_i.array() / sum_exp_xw.array();
+        double loss = -tmp2.array().log().sum();
+
+        loss /= static_cast<double>(num_samples);
+
+        double reg = (W.transpose() * W).array().sum() / 
+            (static_cast<double>(num_samples) * 2.0);
         
-
-
+        return loss + reg * lambda_;
     }
 
     /**
-     * Evaluate the gradient of logistic regression log-likelihood function with the given 
+     * Evaluate the gradient of softmax regression log-likelihood function with the given 
      * parameters using the given batch size from the given point index
      * 
      *      dot(X.T, sigmoid(np.dot(X, W)) - y) / len(y)
     */
-    const VecType gradient(const MatType& X, 
+    const MatType gradient(const MatType& X, 
         const VecType& y,
         const MatType& W) const{
-        
+        std::size_t num_classes = W.cols();
+        std::size_t num_samples = X.rows(), num_features = X.cols();
+
+        MatType grad(num_features, num_classes);
+        MatType xw(num_samples, num_classes);
+        xw = X * W;
+
+        auto tmp1 = xw.array() - xw.maxCoeff();
+        MatType exp_xw = tmp1.array().exp();
+        VecType sum_exp_xw = exp_xw.rowwise().sum();
+
+        // std::cout << "sum_exp_xw: " << sum_exp_xw << std::endl;
+
+        MatType normalized_exp_xw = exp_xw.array().colwise() / sum_exp_xw.array();
+
+        // std::cout << "normalized_exp_xw: " << normalized_exp_xw << std::endl;
+        std::size_t index;
+        for (int i = 0; i < num_samples; ++i) {
+            index = static_cast<std::size_t>(y(i, 0));
+            normalized_exp_xw(i, index) -= 1.0;
+        }
+
+        grad = X.transpose() * normalized_exp_xw;
+        grad.array() /= static_cast<double>(num_samples);
+    
+        MatType reg(num_features, num_classes);
+        reg = W.array() / static_cast<DataType>(num_samples);
+
+        return grad + reg * lambda_;
     };
 
 };
