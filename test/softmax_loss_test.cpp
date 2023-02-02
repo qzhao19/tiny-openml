60b044d497df73c383ae188b0721dae08aaf1ab1
qzhao19 <qi.zhao@outlook.fr>
2023-02-02 23:23:18 +0800

    refine softmax test file

--- test/softmax_loss_test.cpp
+++ test/softmax_loss_test.cpp
@@ -12,6 +12,21 @@ int main() {
     data::loadtxt<MatType, VecType>("../dataset/iris.txt", X, y);
 
     std::size_t num_samples = X.rows(), num_features = X.cols();
-    
+
+    loss::SoftmaxLoss<double> softmax_loss;
+
+    MatType W(num_features, 3);
+    W.setOnes();
+
+    MatType grad;
+    double loss_val = 0.0;
+    loss_val = softmax_loss.evaluate(X, y, W);
+    std::cout << "loss_val" << std::endl;
+    std::cout << loss_val << std::endl;
+
+    grad = softmax_loss.gradient(X, y, W);
+    std::cout << "grad" << std::endl;
+    std::cout << grad << std::endl;
+
 
 }
