8e918008dec7ba2d2c24941cea150b50cbd461ba
qzhao19 <qi.zhao@outlook.fr>
2023-02-06 22:17:01 +0800

    update

--- src/core/optimizer/sgd/update_policies/nesterov_momentum_update.hpp
+++ src/core/optimizer/sgd/update_policies/nesterov_momentum_update.hpp
@@ -21,17 +21,17 @@ public:
     /**
      * SGD with Nesterov Momentum optimization method
     */
-    const VecType update(const VecType& W, const VecType& grad, const double lr) const {
+    const MatType update(const MatType& W, const MatType& grad, const double lr) const {
         std::size_t num_rows = W.rows();
-        VecType V(num_rows);
+        MatType V(num_rows);
         V.setZero();
         
-        VecType prev_V(num_rows);
+        MatType prev_V(num_rows);
         prev_V = V;
 
         V = mu * V + static_cast<DataType>(lr) * grad;
         V = V + mu * (V - prev_V);
-        VecType updated_W = W - V;
+        MatType updated_W = W - V;
         return updated_W;
     };
 
