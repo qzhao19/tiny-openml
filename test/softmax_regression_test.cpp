41fe7b77e7960d1204ddf04af3341d6f906c134a
qzhao19 <qi.zhao@outlook.fr>
2023-02-11 23:46:02 +0800

    add softmax regression test

---
+++ test/softmax_regression_test.cpp
@@ -0,0 +1,28 @@
+#include "../src/methods/linear_model/softmax_regression.hpp"
+using namespace openml;
+
+
+int main() {
+
+    using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+    MatType X;
+    VecType y;
+    data::loadtxt<MatType, VecType>("../dataset/iris.txt", X, y);
+
+    std::cout << "Softmax regression using sgd optimizer" <<std::endl;
+    linear_model::SoftmaxRegression<double> regressor;
+    regressor.fit(X, y);
+
+    VecType y_pred;
+    y_pred = regressor.predict(X);
+    std::cout << "y_pred" << std::endl;
+    std::cout << y_pred << std::endl;
+
+    MatType y_pred_prob;
+    y_pred_prob = regressor.predict_prob(X);
+    std::cout << "y_pred_prob" << std::endl;
+    std::cout << y_pred_prob << std::endl;
+
+};
