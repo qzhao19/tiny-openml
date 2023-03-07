c4577f29e6dffd480f0b7da4c768b0e54f6afc3d
qzhao19 <qi.zhao@outlook.fr>
2023-03-07 22:49:03 +0800

    update

--- src/methods/rule_model/apriori.hpp
+++ src/methods/rule_model/apriori.hpp
@@ -15,7 +15,18 @@ private:
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
     using IdxType = Eigen::Vector<Eigen::Index, Eigen::Dynamic>;
 
+    double min_support_;
+    double min_confidence_;
+
+
+public:
+
+    Apriori(): min_support_(0.4), min_support_(0.6) {};
+    Apriori(double min_support, double min_confidence): 
+        min_support_(min_support), 
+        min_confidence_(min_confidence) {};
     
+    ~Apriori() {};
 
 };
 
