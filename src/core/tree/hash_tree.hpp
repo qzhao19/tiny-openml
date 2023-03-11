0b752ba0070d3ff0312758570bb747f4affb1e94
qzhao19 <qi.zhao@outlook.fr>
2023-03-11 23:33:07 +0800

    update hash tree

--- src/core/tree/hash_tree.hpp
+++ src/core/tree/hash_tree.hpp
@@ -6,9 +6,10 @@
 namespace openml {
 namespace tree {
 
-template<typename DataType, typename NodeType>
+template<typename NodeType, typename DataType>
 class HashTree {
 private:
+
     std::size_t max_leaf_size_;
     std::size_t max_child_size_;
 
@@ -16,47 +17,62 @@ private:
     std::set<std::vector<DataType>> added_;
     std::vector<std::size_t> count_list_;
     std::vector<std::vector<DataType>> itemset_list_;
-    
+
     inline const std::size_t hash(std::size_t num) const {
         return num % max_child_size_;
     }
 
-    void dfs(std::shared_ptr<Node> node, double support) {
 
-    };
+
+protected:
+
+    void insert(std::shared_ptr<NodeType>& node, 
+        std::vector<DataType> itemset, 
+        std::size_t count) {
+        
+        // if current itemset is the last one, just insert it
+        if (node.index == itemset.size()) {
+            node.bucket[itemset] = count;
+        }
+        else {
+            node.bucket[itemset] = (++count);
+        }
+
+        if (!node.is_leaf) {
+            std::size_t key = hash(itemset[node.index]);
+            if (node.children.find(key) == node.children.end()) {
+                node.children[key] = std::make_shared<NodeType>;
+            }
+            insert(node.children[key], itemset, count)
+        }
+        else {
+            if (node.children.find(itemset) == node.children.end()) {
+                node.bucket[itemset] = count;
+            }
+            else {
+                node.bucket[itemset] = (++count);
+            }
+
+            
+        }
+
+
+    }
 
 
 public:
     HashTree(): max_leaf_size_(3), max_child_size_(3) {
-        root_ = std::make_unique<NodeType>();
-        root_->is_leaf = false;
+        // root_ = std::make_shared<NodeType>();
+        // root_->is_leaf = false;
     };
 
     HashTree(std::size_t max_leaf_size, 
         std::size_t max_child_size):
             max_leaf_size_(max_leaf_size), 
             max_child_size_(max_child_size) {
-        root_ = std::make_unique<NodeType>();
-        root_->is_leaf = false;
-    };
-
-    void insert(std::shared_ptr<Node>& node, 
-        std::vector<DataType> itemset, 
-        std::size_t count) {
-        
-        if (node->index == itemset.size()) {
-            if (node->bucket.find(itemset) == node->bucket.end()) {
-                node->bucket[itemset] = count;
-            } 
-            else {
-                node->bucket[itemset] = (++count);
-            }
-        }
-
-
-
-    }
-
+        // root_ = std::make_shared<NodeType>();
+        // root_->is_leaf = false;
+    };   
 
 
 };
