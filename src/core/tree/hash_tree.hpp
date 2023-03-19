377d5990260cef3fd6f3280415cd878c09480183
qzhao19 <qi.zhao@outlook.fr>
2023-03-19 22:49:29 +0800

    update hash tree

--- src/core/tree/hash_tree.hpp
+++ src/core/tree/hash_tree.hpp
@@ -22,10 +22,7 @@ private:
         return num % max_child_size_;
     }
 
-
-
 protected:
-
     void insert(std::shared_ptr<NodeType>& node, 
         std::vector<DataType> itemset, 
         std::size_t count) {
@@ -39,6 +36,7 @@ protected:
             node.bucket[itemset] = (++count);
         }
 
+        // non-leaf node, if current itemset is not in 
         if (!node.is_leaf) {
             key = hash(itemset[node.index]);
             if (node.children.find(key) == node.children.end()) {
@@ -56,7 +54,6 @@ protected:
 
             if (node.bucket.size() > max_leaf_size_) {
                 ++node.index;
-
                 // bucket is a map struct and key is vector
                 for (auto& bucket : node.bucket) {
                     key = hash(bucket.first[node.index]);
@@ -74,9 +71,29 @@ protected:
     }
 
 
+    void dfs(std::shared_ptr<NodeType> node, std::size_t support) {
+        if (node.is_leaf) {
+            for (auto& bucket : node.bucket) {
+                if (bucket.second >= support) {
+                    itemset_list_.emplace_back(bucket.first);
+                    count_list_.emplace_back(bucket.first);
+                }
+            }
+            return ;
+        }
+        else {
+            for (auto& child : node.children) {
+                dfs(child.second, support);
+            }
+        }
+    }
+
+
+
+
 public:
     HashTree(): max_leaf_size_(3), max_child_size_(3) {
-        // root_ = std::make_shared<NodeType>();
+        root_ = std::make_shared<NodeType>();
         // root_->is_leaf = false;
     };
 
@@ -84,10 +101,17 @@ public:
         std::size_t max_child_size):
             max_leaf_size_(max_leaf_size), 
             max_child_size_(max_child_size) {
-        // root_ = std::make_shared<NodeType>();
+        root_ = std::make_shared<NodeType>();
         // root_->is_leaf = false;
     };   
 
+    void build_tree(const std::vector<std::vector<DataType>>& itemsets) {
+        for (std::size_t i = 0; i < itemsets.size(); ++i) {
+            insert(itemsets[i], root_, 0);
+        }
+    }
+
+    
 
 };
 
