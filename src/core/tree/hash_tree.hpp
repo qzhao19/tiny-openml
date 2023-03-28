258bacdd889fbbb82b5ee688dc20a159381360bc
qzhao19 <qi.zhao@outlook.fr>
2023-03-28 23:31:01 +0800

    update

--- src/core/tree/hash_tree.hpp
+++ src/core/tree/hash_tree.hpp
@@ -80,7 +80,6 @@ protected:
                 if (bucket.second >= support) {
                     itemset_list_.emplace_back(bucket.first);
                     count_list_.emplace_back(bucket.second);
-                    // std::cout << "i = " << bucket.second << std::endl;
                 }
             }
             return ;
@@ -90,6 +89,32 @@ protected:
                 dfs(child.second, support);
             }
         }
+    }
+
+public:
+    HashTree(): max_leaf_size_(3), max_child_size_(3) {
+        root_ = std::make_shared<NodeType>();
+    };
+
+    HashTree(std::size_t max_leaf_size, 
+        std::size_t max_child_size):
+            max_leaf_size_(max_leaf_size), 
+            max_child_size_(max_child_size) {
+        root_ = std::make_shared<NodeType>();
+    };   
+
+    void build_tree(const std::vector<std::vector<DataType>>& itemsets) {
+        for (std::size_t i = 0; i < itemsets.size(); ++i) {
+            insert(root_, itemsets[i], 0);
+        }
+    }
+
+    void compute_frequency_itemsets(std::size_t support, 
+        std::vector<std::size_t>& count_list,
+        std::vector<std::vector<DataType>>& itemset_list) {
+        dfs(root_, support);
+        count_list = count_list_;
+        itemset_list = itemset_list_;
     }
 
     void add_support(std::shared_ptr<NodeType> node, 
@@ -139,33 +164,6 @@ protected:
                 }
             }
         }
-
-public:
-    HashTree(): max_leaf_size_(3), max_child_size_(3) {
-        root_ = std::make_shared<NodeType>();
-    };
-
-    HashTree(std::size_t max_leaf_size, 
-        std::size_t max_child_size):
-            max_leaf_size_(max_leaf_size), 
-            max_child_size_(max_child_size) {
-        root_ = std::make_shared<NodeType>();
-    };   
-
-    void build_tree(const std::vector<std::vector<DataType>>& itemsets) {
-        for (std::size_t i = 0; i < itemsets.size(); ++i) {
-            insert(root_, itemsets[i], 0);
-        }
-    }
-
-    void compute_frequency_itemsets(std::size_t support) {
-        dfs(root_, support);
-
-        // for (auto i : count_list_) {
-        //     std::cout << "i = " << i << std::endl;
-        // }
-
-    }
 };
 
 }
