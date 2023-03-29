a1112e1a945ad4622c450b80c171871d1842e5ea
qzhao19 <qi.zhao@outlook.fr>
2023-03-29 23:57:36 +0800

    create preprocessing

---
+++ src/core/preprocessing/transaction_encoder.hpp
@@ -0,0 +1,26 @@
+#ifndef CORE_PREPPROCESSING_TRANSACTION_ENCODER_HPP
+#define CORE_PREPPROCESSING_TRANSACTION_ENCODER_HPP
+#include "../../../prereqs.hpp"
+#include "../../../core.hpp"
+using namespace openml;
+
+namespace openml {
+namespace preprocessing {
+
+
+template<typename DataType>
+class TransactionEncoder {
+
+
+
+
+
+
+
+
+};
+
+}
+}
+
+#endif /*CORE_PREPPROCESSING_TRANSACTION_ENCODER_HPP*/
