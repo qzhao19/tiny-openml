983eb92c1f7f0bdc92afa99aa137db271fa5366b
qzhao19 <qi.zhao@outlook.fr>
2023-04-01 23:43:55 +0800

    add transaction-encoder

--- src/core/preprocessing/transaction_encoder.hpp
+++ src/core/preprocessing/transaction_encoder.hpp
@@ -10,10 +10,41 @@ namespace preprocessing {
 
 template<typename DataType>
 class TransactionEncoder {
+private:
+    std::set<DataType> cols_;
+    std::map<DataType, std::size_t> cols_map_;
+
+    void fit_data(const std::vector<std::vector<DataType>>& X) {
+
+        for (std::size_t i = 0; i < X.size(); ++i) {
+            for (std::size_t j = 0; j < X[i].size(); ++j) {
+                cols_.insert(X[i][j]);
+            }
+        }
+
+        std::size_t index = 0;
+        for (auto it = ss.begin(); it != ss.end(); it++) {
+            cols_map_[index] = it;
+            ++index;
+        }
+
+    }
+
+
+    void transform_data(const std::vector<std::vector<DataType>>& X, bool sparse) {
+
+        if (sparse) {
+            
 
 
 
 
+
+        }
+
+
+
+    }
 
 
 
