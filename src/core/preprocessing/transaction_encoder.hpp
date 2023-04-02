f834ab1452737bb4b914ea0465e37fecde9264d8
qzhao19 <qi.zhao@outlook.fr>
2023-04-02 23:54:00 +0800

    update

--- src/core/preprocessing/transaction_encoder.hpp
+++ src/core/preprocessing/transaction_encoder.hpp
@@ -7,7 +7,6 @@ using namespace openml;
 namespace openml {
 namespace preprocessing {
 
-
 template<typename DataType>
 class TransactionEncoder {
 private:
@@ -21,29 +20,34 @@ private:
                 cols_.insert(X[i][j]);
             }
         }
-
         std::size_t index = 0;
         for (auto it = ss.begin(); it != ss.end(); it++) {
             cols_map_[index] = it;
             ++index;
         }
-
     }
 
 
     void transform_data(const std::vector<std::vector<DataType>>& X, bool sparse) {
 
         if (sparse) {
-            
-
+            std::vector<std::size_t> indices;
+            for (std::size_t i = 0; i < X.size(); ++i) {
+                for (std::size_t j = 0; j < X[i].size(); ++j) {
+                    auto col_idx = cols_map_[X[i]]
+                    indices.emplace_back(col_idx);
+                }
+                indptr.emplace_back(indices.size());
+            }
+            auto non_sparse_values = indices.size()
+            auto array = csr_matrix(non_sparse_values, indices, indptr)
+        }
+        else{
 
 
 
 
         }
-
-
-
     }
 
 
