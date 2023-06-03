68d95c7e496a095cf867e2f52dd81778e1dda34e
qzhao19 <qi.zhao@outlook.fr>
2023-06-03 22:39:03 +0800

    add example

--- test/apriori_test.cpp
+++ test/apriori_test.cpp
@@ -1,18 +1,14 @@
 #include "../src/methods/rule_model/apriori.hpp"   
 using namespace openml;
 
-
 int main() {
-
-    using SpMatType = Eigen::SparseMatrix<std::size_t>;
-
     std::vector<std::vector<std::size_t>> X;
     data::loadtxt<std::size_t>("../dataset/input-1.txt", X);
+    // X = {{2, 5, 4, 9}, {1, 6}, {7, 3}, {3, 2, 1, 8, 5}, {2, 4, 7, 9}};
 
     rule_model::Apriori<std::size_t> apriori;
-
-    apriori.fit(X);
-
+    apriori.fit_transform(X);
+    apriori.print_rules();
 }
 
 
