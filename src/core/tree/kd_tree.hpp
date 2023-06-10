1180a9890d36fd8832f9e7313a67ef14bd626269
qzhao19 <qi.zhao@outlook.fr>
2023-06-10 23:55:24 +0800

    update

--- src/core/tree/kd_tree.hpp
+++ src/core/tree/kd_tree.hpp
@@ -13,47 +13,19 @@ private:
     using VecType = Eigen::Matrix<DataType, Eigen::Dynamic, 1>;
     using IdxVecType = Eigen::Vector<Eigen::Index, Eigen::Dynamic>;
 
-    std::size_t p_;
-    std::size_t num_neighbors_;
+    std::size_t leaf_size_;
     std::string metric_;
     std::shared_ptr<NodeType> root_;
-
-
-    struct Node {
-        bool is_leaf;
-        IdxVecType data_indices_per_node;
-        std::weak_ptr<Node> parent;
-        std::shared_ptr<Node> left_child;
-        std::shared_ptr<Node> right_child;
-
-        Node():is_leaf(true),
-            data_indices_per_node(IdxVecType()),
-            parent(std::weak_ptr<Node>()),
-            left_child(std::shared_ptr<Node>()), 
-            right_child(std::shared_ptr<Node>()) {};
-
-        Node(bool is_leaf_,
-            IdxVecType data_indices_per_node_): 
-            is_leaf(is_leaf_),
-            data_indices_per_node(data_indices_per_node_),
-                parent(std::weak_ptr<Node>()),
-                left_child(std::shared_ptr<Node>()), 
-                right_child(std::shared_ptr<Node>()) {};
-
-        ~Node(){};
-    };
+    
     
 
 public:
-    KDTree(): num_neighbors_(15), 
-        p_(1), 
+    KDTree(): leaf_size_(40), 
         metric_("minkowski") {};
     
 
-    KDTree(std::size_t num_neighbors, 
-        std::size_t p, 
-        std::string metric): num_neighbors_(num_neighbors), 
-            p_(p), 
+    KDTree(std::size_t leaf_size, 
+        std::string metric): leaf_size_(leaf_size), 
             metric_(metric) {};
     
 
