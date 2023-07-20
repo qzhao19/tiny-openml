58f924d56eb53c38e70750969fe2c5a32ed7aec0
qzhao19 <qi.zhao@outlook.fr>
2023-07-20 22:54:15 +0800

    update

--- src/core/tree/kd_tree.hpp
+++ src/core/tree/kd_tree.hpp
@@ -6,8 +6,83 @@
 namespace openml {
 namespace tree {
 
+template<typename DataType>
+class KDTree {
+private:
+    struct KDTreeNode {
+        int left;
+        int right;
+        std::shared_ptr<std::vector<std::size_t>> indices;
+        std::shared_ptr<std::vector<std::vector<DataType>>> data;
+        std::shared_ptr<std::vector<std::vector<DataType>>> left_hyper_rect;
+        std::shared_ptr<std::vector<std::vector<DataType>>> right_hyper_rect;
+
+        Node(): left(-1), 
+            right(-1), 
+            indices(nullptr), 
+            data(nullptr), 
+            left_hyper_rect(nullptr), 
+            right_hyper_rect(nullptr) {};
+
+        Node(int left_, 
+            int right_, 
+            std::shared_ptr<std::vector<std::size_t>> indices_, 
+            std::shared_ptr<std::vector<std::vector<DataType>>> data_, 
+            std::shared_ptr<std::vector<std::vector<DataType>>> left_hyper_rect_, 
+            std::shared_ptr<std::vector<std::vector<DataType>>> right_hyper_rect_): 
+                left(left_), 
+                right(right_), 
+                data(data_), 
+                left_hyper_rect(left_hyper_rect_), 
+                right_hyper_rect(right_hyper_rect_) {};
+    }
+
+    std::size_t leaf_size_;
+    std::vector<std::vector<DataType>> data_;
+    std::vector<KDTreeNode> tree_;
+
+protected:
+
+    std::size_t find_partition_axis(const std::vector<std::vector<DataType>>& data) {
+
+        std::size_t num_samples = data.size();
+        std::size_t num_features = data[0].size();
+        std::vector<DataType> range_bounds(num_features);
+        std::vector<DataType> lower_bounds(num_features, ConstType<DataType>::infinity());
+        std::vector<DataType> upper_bounds(num_features, -ConstType<DataType>::infinity());
+
+        for (std::size_t i = 0; i < num_features; ++i) {
+            for (std::size_t j = 0; j < num_features; ++j) {
+                lower_bounds[j] = std::min(lower_bounds[j], data[i][j]);
+                upper_bounds[j] = std::max(upper_bounds[j], data[i][j]);
+            }
+        }
+
+        std::size_t partition_axis = 0;
+        for (std::size_t i = 0; i < num_features; ++i) {
+            range_bounds[i] = upper_bounds[i] - lower_bounds[i]
+            if (range_bounds[i] > range_bounds[partition_axis]) {
+                partition_axis = i;
+            }
+        }
+
+        return partition_axis;
+    }
 
 
+
+
+
+
+public:
+    KDTree(const std::vector<std::vector<DataType>>& data, 
+        std::size_t leaf_size): data_(data), leaf_size_(leaf_size) {};
+
+    KDTree(const std::vector<std::vector<DataType>>& data): 
+        data_(data), leaf_size_(10) {};
+
+    ~KDTree() {};
+};
 
 
 }
