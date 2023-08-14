5b7fb1d6dfcb7f6c3544e05ff595ed130152040e
qzhao19 <qi.zhao@outlook.fr>
2023-08-14 22:36:11 +0800

    update

--- src/core/math/dist.hpp
+++ src/core/math/dist.hpp
@@ -7,32 +7,30 @@ namespace openml {
 namespace math {
 
 /**
- * gaussian probability distribution
- * -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * pi * v))
- * 
- * @param x DataType, input value
- * @param u DataType, mean
- * @param v DataType, variance 
+ * Compute the L2 euclidean distances between the vectors in X and Y.
 */
-// template<typename DataType>
-// double gaussian_fn(const DataType& x, 
-//     const DataType& u, 
-//     const DataType& v) {
-    
-//     // retval = -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * arma::datum::pi * v));
-//     double retval = -1.0 * std::pow((x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * arma::datum::pi * v));
-
-//     return retval;
-// };
-
 template <typename DataType>
-double euclidean_distance(const std::vector<DataType>& a, const std::vector<DataType>& b) {
+DataType euclidean_distance(const std::vector<DataType>& a, const std::vector<DataType>& b) {
     std::vector<DataType> aux;
     std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(aux),
                    [](DataType x1, DataType x2) { return std::pow((x1 - x2), 2); });
     aux.shrink_to_fit();
     return std::sqrt(std::accumulate(aux.begin(), aux.end(), 0.0));
 }
+
+/**
+ * Compute the L1 manhattan distances between the vectors in X and Y.
+*/
+template <typename DataType>
+DataType manhattan_distance(const std::vector<DataType>& a, const std::vector<DataType>& b) {
+    std::vector<DataType> aux;
+    std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(aux),
+                   [](DataType x1, DataType x2) { return std::abs(x1 - x2); });
+    aux.shrink_to_fit();
+    return std::accumulate(aux.begin(), aux.end(), 0.0)
+}
+
+
 
 }
 }
