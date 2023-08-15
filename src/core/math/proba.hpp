1850314182d543807e8d68f38dd5b99e5da034d6
qzhao19 <qi.zhao@outlook.fr>
2023-08-15 23:55:33 +0800

    update

---
+++ src/core/math/proba.hpp
@@ -0,0 +1,33 @@
+#ifndef CORE_MATH_DISTS_HPP
+#define CORE_MATH_DISTS_HPP
+#include "../../prereqs.hpp"
+
+namespace openml {
+
+namespace math {
+
+
+/**
+ * gaussian probability distribution
+ * -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * pi * v))
+ * 
+ * @param x DataType, input value
+ * @param u DataType, mean
+ * @param v DataType, variance 
+*/
+// template<typename DataType>
+// double gaussian_fn(const DataType& x, 
+//     const DataType& u, 
+//     const DataType& v) {
+    
+//     // retval = -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * arma::datum::pi * v));
+//     double retval = -1.0 * std::pow((x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * arma::datum::pi * v));
+//     return retval;
+// };
+
+
+
+}
+}
+
+#endif
