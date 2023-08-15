e38febdee5266b2cc7112a9f363825900f44b1e2
qzhao19 <qi.zhao@outlook.fr>
2023-08-15 23:54:57 +0800

    update

--- src/core/metric.hpp
+++ src/core/metric.hpp
@@ -43,6 +43,33 @@ double explained_variance_score(const VecType& y_true,
     return static_cast<double>(retval(0, 0));
 };
 
+/**
+ * Compute the L2 euclidean distances between the vectors in X and Y.
+*/
+template <typename DataType>
+DataType euclidean_distance(const std::vector<DataType>& a, const std::vector<DataType>& b) {
+    std::vector<DataType> aux;
+    std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(aux),
+                   [](DataType x1, DataType x2) { return std::pow((x1 - x2), 2); });
+    aux.shrink_to_fit();
+    return std::sqrt(std::accumulate(aux.begin(), aux.end(), 0.0));
+}
+
+/**
+ * Compute the L1 manhattan distances between the vectors in X and Y.
+*/
+template <typename DataType>
+DataType manhattan_distance(const std::vector<DataType>& a, const std::vector<DataType>& b) {
+    std::vector<DataType> aux;
+    std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(aux),
+                   [](DataType x1, DataType x2) { return std::abs(x1 - x2); });
+    aux.shrink_to_fit();
+    return std::accumulate(aux.begin(), aux.end(), 0.0)
+}
+
+
+
+
 
 }
 }
