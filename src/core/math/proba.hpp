3dadafa47a84614edeefbc05b1c0b7a4d59365d8
qzhao19 <qi.zhao@outlook.fr>
2023-08-23 22:34:08 +0800

    update

--- src/core/math/proba.hpp
+++ src/core/math/proba.hpp
@@ -15,15 +15,14 @@ namespace math {
  * @param u DataType, mean
  * @param v DataType, variance 
 */
-// template<typename DataType>
-// double gaussian_fn(const DataType& x, 
-//     const DataType& u, 
-//     const DataType& v) {
+template<typename DataType>
+double gaussian_fn(const DataType& x, 
+    const DataType& u, 
+    const DataType& v) {
     
-//     // retval = -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * arma::datum::pi * v));
-//     double retval = -1.0 * std::pow((x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * arma::datum::pi * v));
-//     return retval;
-// };
+    // retval = -(x - u) ** 2.0 / (2.0 * v) - log(sqrt(2.0 * arma::datum::pi * v));
+    return retval = -1.0 * std::pow((x - u), 2) / (2.0 * v) - std::log(std::sqrt(2.0 * M_PI * v));
+};
 
 
 
