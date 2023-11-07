9cfc6571c0ec1faae83e62e303f9ab1827df3dc4
qzhao19 <qi.zhao@outlook.fr>
2023-11-07 23:35:14 +0800

    update

--- test/kd_tree_test.cpp
+++ test/kd_tree_test.cpp
@@ -3,18 +3,35 @@ using namespace openml;
 
 
 int main() {
-    std::vector<std::vector<double>> X;
-    std::vector<double> y;
-    data::loadtxt<double>("../dataset/iris.txt", X, y);
+    using MatType = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using RowVecType = Eigen::Matrix<double, 1, Eigen::Dynamic>;
+    using ColVecType = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+    using IdxMatType = Eigen::Matrix<Eigen::Index, Eigen::Dynamic, Eigen::Dynamic>;
 
-    tree::KDTree<double> kd_tree(X);
+    MatType X, X_train, X_test;
+    ColVecType y;
 
-    kd_tree.test();
+    data::loadtxt<MatType, ColVecType>("../dataset/iris.txt", X, y);
 
+    X_train = X.topRows(125);
+    X_test = X.bottomRows(25);
+    tree::KDTree<double> kd_tree(X_train);
+    
+    MatType distances1;
+    IdxMatType indices1;
+    std::tie(distances1, indices1) = kd_tree.query(X_test, 4);
+    std::cout << "distances1" << std::endl;
+    std::cout << distances1 << std::endl;
+    std::cout << "indices1" << std::endl;
+    std::cout << indices1 << std::endl;
+
+    // std::vector<std::pair<double, std::size_t>> knn;
+    std::vector<std::vector<std::pair<double, std::size_t>>> knn;
+    knn = kd_tree.query_radius(X_test, 0.5);
+    for (auto nn : knn) {
+        for (auto n : nn) {
+            std::cout << n.first << " " << n.second << " ";
+        }
+        std::cout << std::endl;
+    }
 }
-
-
-
-
-
-
