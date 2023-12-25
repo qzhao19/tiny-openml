524b84c83e44703d75dba8191ac7facacb386071
qzhao19 <qi.zhao@outlook.fr>
2023-12-25 23:17:17 +0800

    update

--- src/methods/neighbors/k_nearest_neighbors.hpp
+++ src/methods/neighbors/k_nearest_neighbors.hpp
@@ -24,45 +24,54 @@ private:
     std::string solver_;
     std::string metric_;
 
+    MatType X_;
     ColVecType y_;
     std::unique_ptr<tree::KDTree<DataType>> tree_;
 
 protected:
-    void brute_force(const MatType& X, const ColVecType& y, const MatType& samples) {
+    DataType brute_force(const MatType& X, const ColVecType& y, const RowVecType& samples) {
         
         std::vector<DataType> neighbors;
-        std::vector<std::pair<DataType, DataType>> dist_map;
+        std::vector<std::pair<double, DataType>> lookup_dist;
 
         for (std::size_t i = 0; i < X.row(); ++i) {
-            // auto current = X.rows(i);
-            // auto label = y(i, 0);
-            // auto distance;
-            for (std::size_t i = 0; i < samples.rows(); ++i) {
-                if (metric_ == "euclidean") {
-                    distance = (current - sample).template lpNorm<2>();
-                }
-                else if (metric_ == "manhattan") {
-                    distance = (current - sample).template lpNorm<1>();
-                }
-                else {
-                    std::ostringstream err_msg;
-                    err_msg << "Only support euclidean and manhattan distance." << std::endl;
-                    throw std::invalid_argument(err_msg.str());
-
-                }
+            RowVecType current = X.rows(i);
+            DataType label = y(i, 0);
+            double distance;
+            
+            if (metric_ == "euclidean") {
+                distance = (current - sample).template lpNorm<2>();
             }
-            dist_map.emplace_back(distance, label);
-        }
+            else if (metric_ == "manhattan") {
+                distance = (current - sample).template lpNorm<1>();
+            }
+            else {
+                std::ostringstream err_msg;
+                err_msg << "Only support euclidean and manhattan distance." << std::endl;
+                throw std::invalid_argument(err_msg.str());
 
-        std::sort(dist_map.begin(), dist_map.end());
+            }
+            lookup_dist.emplace_back(distance, label);
+        }
+        std::sort(lookup_dist.begin(), lookup_dist.end());
         for (std::size_t i = 0; i < num_neighbors_; i++) {
-            auto label = dist_map.at(i).second;
+            auto label = lookup_dist.at(i).second;
             neighbors.push_back(label);
         }
         std::unordered_map<DataType, std::size_t> frequency;
         for (auto neighbor : neighbors) {
             ++frequency[neighbor];
         }
+        std::pair<DataType, std::size_t> pred_label;
+        pred_label.first = -1;
+        pred_label.second = -1;
+        for (auto& kv : frequency) {
+            if (kv.second > pred_label.second) {
+                pred_label.second = kv.second;
+                pred_label.first = kv.first;
+            }
+        }
+        return pred_label.first;
 
     }
       
@@ -84,29 +93,49 @@ public:
 
     
     void fit(const MatType& X, const ColVecType& y) {
-        // call kdTree via a function pionter
-        tree_ = std::make_unique<tree::KDTree<DataType>>(
-            X, leaf_size_, metric_
-        );
+        
+        if (solver_ == "kdtree") {
+            // call kdTree via a function pionter
+            tree_ = std::make_unique<tree::KDTree<DataType>>(
+                X, leaf_size_, metric_
+            );
+            
+        }
+        else if (solver_ == "brute") {
+            X_ = X;
+        }
         y_ = y;
+        
     }
 
     const ColVecType predict(const MatType& X) {
-        MatType neighbor_dist;
-        IdxMatType neighbor_ind;
-        std::tie(neighbor_dist, neighbor_ind) = tree_->query(X, num_neighbors_);
+        ColVecType pred_label;
+        if (solver_ == "kdtree") {
+            MatType neighbor_dist;
+            IdxMatType neighbor_indice;
+            std::tie(neighbor_dist, neighbor_indice) = tree_->query(X, num_neighbors_);
 
-        std::size_t num_samples = X.rows();
-        MatType pred_ind(num_samples, num_neighbors_);
-        for (std::size_t i = 0; i < num_samples; ++i) {
-            pred_ind.row(i) = y_(neighbor_ind.row(i), Eigen::all).transpose();
+            std::size_t num_samples = X.rows();
+            MatType pred_indice(num_samples, num_neighbors_);
+            for (std::size_t i = 0; i < num_samples; ++i) {
+                pred_indice.row(i) = y_(neighbor_indice.row(i), Eigen::all).transpose();
+            }
+
+            MatType mode, count;
+            std::tie(mode, count) = math::mode<MatType>(pred_indice, 0);
+
+            pred_label = mode;
         }
-
-        MatType mode, count;
-        std::tie(mode, count) = math::mode<MatType>(pred_ind, 0);
-
-        return mode;
-    
+        else if (solver_ == "brute") {
+            std::size_t num_samples = X.rows();
+            pred_label.resize(num_samples);
+            for (std::size_t i = 0; i < num_samples; ++i) {
+                DataType lable = brute_force(X_, y_, X.row(i));
+                pred_label(i, 0) = lable;
+            }
+        }
+        
+        return pred_label;
     }
 
 };
